8c510f73839fbfa280c3d2fca70cfd6c34ded6ca
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-02-20 17:57:23 +0800

    初始化工程,准备用 AVL 去实现

---
+++ AlgorithmicCrush/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  unsigned int n;
+  unsigned long long m;
+
+  scanf("%u %llu", &n, &m);
+
+  for (unsigned long long i = 0; i < m; i++) {
+    unsigned int a, b, k;
+
+    scanf("%u %u %u", &a, &b, &k);
+  }
+
+  return 0;
+}
