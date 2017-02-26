765ae01e71d1e83e2f894d95a827ce9ec942d4f1
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-02-26 18:46:41 +0800

    拿满70分

--- AndXorOr/main.c
+++ AndXorOr/main.c
@@ -37,26 +37,23 @@ unsigned int pop(struct mystk * stk) {
 }
 
 int main() {
+//  FILE* fstream = fopen("input.txt", "r");
   unsigned int n;
 
   scanf("%u", &n);
+//  fscanf(fstream, "%u", &n);
 
   struct mystk * as = malloc(sizeof(struct mystk));     // array A
   as->ptr = malloc(n * sizeof(unsigned int));
   memset(as->ptr, 0, n * sizeof(unsigned int));
   as->pos = -1;
 
-  struct mystk * tmps = malloc(sizeof(struct mystk));   // 计算过程中临时的 stack
-  tmps->ptr = malloc(n * sizeof(unsigned int));
-  memset(tmps->ptr, 0, n * sizeof(unsigned int));
-  tmps->pos = -1;
-
   unsigned int max = 0;
-  unsigned int min1 = 0, min2 = 0;
 
   for (unsigned int i = 0; i < n; i++) {
     unsigned int a;
     scanf("%u", &a);
+//    fscanf(fstream, "%u", &a);
 
     unsigned int left = pop(as);
     unsigned int lleft = top(as);
@@ -67,100 +64,42 @@ int main() {
     else if (0 == lleft) {
       push(as, left);
       push(as, a);
-      max = left ^ a;
-//      printf("1: max is %u\n", max);
-      if (a < left) {
-        min1 = a;
-        min2 = left;
-      }
-      else {
-        min1 = left;
-        min2 = a;
-      }
-    }
-    else {
-      unsigned int min = left;
-      unsigned int tmp = min ^ a;
+
+      unsigned int tmp = left ^ a;
       if (max < tmp) {
         max = tmp;
-//        printf("2: max is %u, min is %u, a is %u\n", max, min, a);
       }
-      if (a <= min1 || a <= min2) {
-        while (left > a) {
-          push(tmps, left);
-
-          left = pop(as);
-          if (0 == left) {
-            break;
-          }
-          if (left < min) {
-            min = left;
-            tmp = min ^ a;
-            if (max < tmp) {
-              max = tmp;
-//              printf("3: max is %u, min is %u, a is %u\n", max, min, a);
-            }
-          }
-        }
-        if (left) {
-          push(as, left);
-          if (min <= min1) {
-            min2 = min1;
-            min1 = min;
-          }
-          else {
-            min2 = min;
-          }
-        }
+      //      printf("1: max is %u\n", max);
+    }
+    else {
+      unsigned int tmp = left ^ a;
+      if (max < tmp) {
+        max = tmp;
+      }
+      if (a > left) {
+        push(as, left);
+        push(as, a);
       }
       else {
-        unsigned int cnt = 0;
-
-        if (left <= a) {
-          cnt ++;
-        }
-
-        while (cnt < 2) {
-          push(tmps, left);
-
-          left = pop(as);
-          if (0 == left) {
+        // a <= left
+        while(left = pop(as)) {
+          tmp = left ^ a;
+          if (max < tmp) {
+            max = tmp;
+          }
+          if (left <= a) {
+            push(as, left);
             break;
           }
-
-          if (left <= a) {
-            cnt ++;
-            if (2 == cnt) {
-              break;
-            }
-          }
-
-          if (left < min) {
-            min = left;
-            tmp = min ^ a;
-            if (max < tmp) {
-              max = tmp;
-//              printf("5: max is %u, min is %u, a is %u\n", max, min, a);
-            }
-          }
         }
 
-        if (left) {
-          push(as, left);
-        }
+        push(as, a);
       }
-
-      while (tmp = pop(tmps)) {
-        push(as, tmp);
-      }
-      push(as, a);
     }
   }
 
   printf("%u\n", max);
 
-  free(tmps->ptr);
-  free(tmps);
   free(as->ptr);
   free(as);
 
