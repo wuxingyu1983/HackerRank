ca0299a33767622e3c1242a1eb15144e625db762
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-02-26 13:08:48 +0800

    初步提交

--- AndXorOr/main.c
+++ AndXorOr/main.c
@@ -2,28 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-int pos = -1;
+struct mystk {
+  unsigned int *  ptr;
+  int             pos;
+};
 
-unsigned int top(unsigned int * stk) {
+unsigned int top(struct mystk * stk) {
   unsigned int ret = 0;
 
-  if (0 <= pos) {
-    ret = stk[pos];
+  if (0 <= stk->pos) {
+    ret = stk->ptr[stk->pos];
   }
 
   return ret;
 }
 
-void push(unsigned int * stk, unsigned int elem) {
-  stk[++ pos] = elem;
+void push(struct mystk * stk, unsigned int elem) {
+  int p = stk->pos;
+  stk->ptr[++ p] = elem;
+  stk->pos = p;
 }
 
-unsigned int pop(unsigned int * stk) {
+unsigned int pop(struct mystk * stk) {
   unsigned int ret = 0;
+  int p = stk->pos;
 
-  if (0 <= pos) {
-    ret = stk[pos];
-    stk[pos --] = 0;
+  if (0 <= p) {
+    ret = stk->ptr[p];
+    stk->ptr[p--] = 0;
+    stk->pos = p;
   }
 
   return ret;
@@ -34,20 +41,110 @@ int main() {
 
   scanf("%u", &n);
 
-  unsigned int * as;    // array A
-  as = malloc(n * sizeof(unsigned int));
-  memset(as, 0, n * sizeof(unsigned int));
+  struct mystk * as = malloc(sizeof(struct mystk));     // array A
+  as->ptr = malloc(n * sizeof(unsigned int));
+  memset(as->ptr, 0, n * sizeof(unsigned int));
+  as->pos = -1;
 
-  unsigned int * tmps;   // 计算过程中临时的 stack
-  tmps = malloc(n * sizeof(unsigned int));
-  memset(tmps, 0, n * sizeof(unsigned int));
+  struct mystk * tmps = malloc(sizeof(struct mystk));   // 计算过程中临时的 stack
+  tmps->ptr = malloc(n * sizeof(unsigned int));
+  memset(tmps->ptr, 0, n * sizeof(unsigned int));
+  tmps->pos = -1;
 
   unsigned int max = 0;
+  unsigned int min1 = 0, min2 = 0;
 
   for (unsigned int i = 0; i < n; i++) {
     unsigned int a;
     scanf("%u", &a);
 
+    unsigned int left = pop(as);
+    unsigned int lleft = top(as);
+
+    if (0 == left) {
+      push(as, a);
+    }
+    else if (0 == lleft) {
+      push(as, left);
+      push(as, a);
+      max = left ^ a;
+      printf("1: max is %u\n", max);
+      if (a < left) {
+        min1 = a;
+        min2 = left;
+      }
+      else {
+        min1 = left;
+        min2 = a;
+      }
+    }
+    else {
+      unsigned int min = left;
+      unsigned int tmp = min ^ a;
+      if (max < tmp) {
+        max = tmp;
+        printf("2: max is %u, min is %u, a is %u\n", max, min, a);
+      }
+      if (a <= min1 || a <= min2) {
+        while (left > a) {
+          push(tmps, left);
+
+          left = pop(as);
+          if (0 == left) {
+            break;
+          }
+          if (left < min) {
+            min = left;
+            tmp = min ^ a;
+            if (max < tmp) {
+              max = tmp;
+              printf("3: max is %u, min is %u, a is %u\n", max, min, a);
+            }
+          }
+        }
+        min = left;
+        tmp = min ^ a;
+        if (max < tmp) {
+          max = tmp;
+          printf("4: max is %u, min is %u, a is %u\n", max, min, a);
+        }
+        if (min <= min1) {
+          min2 = min1;
+          min1 = min;
+        }
+        else {
+          min2 = min;
+        }
+      }
+      else {
+        unsigned int cnt = 0;
+        while (cnt < 2) {
+          push(tmps, left);
+
+          left = pop(as);
+          if (0 == left) {
+            break;
+          }
+          if (left < min) {
+            min = left;
+            tmp = min ^ a;
+            if (max < tmp) {
+              max = tmp;
+              printf("5: max is %u, min is %u, a is %u\n", max, min, a);
+            }
+          }
+
+          if (left <= a) {
+            cnt ++;
+          }
+        }
+      }
+
+      while (tmp = pop(tmps)) {
+        push(as, tmp);
+      }
+      push(as, a);
+    }
   }
 
   printf("%u\n", max);
