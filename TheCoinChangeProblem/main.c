070148abfeece0881a33fdcaf82cd740ff7fc4f6
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-03-06 01:40:33 +0800

    拿满60分

--- TheCoinChangeProblem/main.c
+++ TheCoinChangeProblem/main.c
@@ -3,11 +3,42 @@
 #include <math.h>
 #include <stdlib.h>
 
+unsigned int m, n;
+unsigned int * c;
+int * * p;
+
+unsigned long long getLen(int sum, unsigned int max) {
+  unsigned long long ret = 0;
+
+  if (0 > sum) {
+    return 0;
+  }
+
+  if (0 <= p[max - 1][sum - 1]) {
+    ret = p[max - 1][sum - 1];
+  }
+  else {
+    for (unsigned int i = 0; i < max; i ++) {
+      if (sum < c[i]) {
+        break;
+      }
+      else if (sum == c[i]) {
+        ret += 1;
+        break;
+      }
+      else {
+        ret += getLen(sum - c[i], i + 1);
+      }
+    }
+    p[max - 1][sum - 1] = ret;
+  }
+
+  return ret;
+}
+
 int main() {
-  unsigned int m, n;
   scanf("%u %u", &n, &m);
 
-  unsigned int * c;
   c = malloc(sizeof(unsigned int) * m);
   for (unsigned int i = 0; i < m; i ++) {
     unsigned int tmp;
@@ -26,6 +57,17 @@ int main() {
     c[j] = tmp;
   }
 
+  p = malloc(sizeof(int *) * m);
+  for (unsigned int i = 0; i < m; i ++) {
+    p[i] = malloc(sizeof(int) * n);
+    memset(p[i], -1, sizeof(int) * n);
+  }
+
+//  p[0][c[0] - 1] = 1;
+
+  unsigned long long l = getLen(n, m);
+
+  printf("%llu\n", l);
 
   return 0;
 }
