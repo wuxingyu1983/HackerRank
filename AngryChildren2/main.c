3d781d5c17cd8952da688f1fb3a9d41e0b66fbbf
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-03-07 21:19:31 +0800

    拿满50分,和动态规划没啥关系

--- AngryChildren2/main.c
+++ AngryChildren2/main.c
@@ -33,26 +33,18 @@ int main() {
   double TheTimes;
   start=clock();
 
-  FILE* fstream = fopen("input", "r");
+//  FILE* fstream = fopen("input", "r");
 
   int N, K;
-//  scanf("%d", &N);
-//  scanf("%d", &K);
-  fscanf(fstream, "%d", &N);
-  fscanf(fstream, "%d", &K);
+  scanf("%d", &N);
+  scanf("%d", &K);
+//  fscanf(fstream, "%d", &N);
+//  fscanf(fstream, "%d", &K);
   int arr[N];
 
-  unsigned long long * s;
-  unsigned long long * f;
-
-  s = malloc(sizeof(unsigned long long) * N);
-  memset(s, 0, sizeof(unsigned long long) * N);
-  f = malloc(sizeof(unsigned long long) * N);
-  memset(f, 0, sizeof(unsigned long long) * N);
-
   for(int i = 0; i < N; i++) {
-    fscanf(fstream, "%d", &arr[i]);
-    //    scanf("%d",&tmp);
+//    fscanf(fstream, "%d", &arr[i]);
+    scanf("%d",&arr[i]);
   }
 
   // quick sort
@@ -60,35 +52,46 @@ int main() {
 
   finish=clock();
   TheTimes=(double)(finish-start)/CLOCKS_PER_SEC;
-  printf("after sort : %f\n",TheTimes);
+//  printf("after sort : %f\n",TheTimes);
 
-  unsigned long long unfair = 0;
-  for (unsigned int i = 1; i < K; i ++) {
-    for (unsigned int j = 0; j < N - i; j ++) {
-      s[j] = s[j] + i * (arr[j + i - 1] - arr[j + i]);
-      f[j] = f[j] + s[j];
+  long long unfair = 0;
+  long long min = 0;
+  long long offset = 0;
+  long long tmp = 0;
+  long long * d = malloc(sizeof(long long) * N);
+  memset(d, 0, sizeof(long long) * N);
 
-      if (K - 1 == i) {
-        if (0 == j) {
-          unfair = f[j];
-        }
-        else {
-          if (unfair > f[j]) {
-            unfair = f[j];
-          }
-        }
+  for (int i = 1; i < N; i ++) {
+    d[i] = arr[i - 1] - arr[i];
+    if (i < K) {
+      unfair += d[i] * (K - i) * i;
+      min = unfair;
+      offset += d[i] * (K - 1 - 2 * (i - 1));
+    }
+    else {
+      offset -= d[i] * (K - 1);
+      if (i > K) {
+        offset -= d[i - K] * (K - 1);
+        offset += 2 * tmp;
       }
+      unfair -= offset;
+      if (min > unfair) {
+        min = unfair;
+      }
+    }
+    if (i > 1) {
+      tmp += d[i];
+    }
+    if (i > K) {
+      tmp -= d[i - K + 1];
     }
   }
 
-  printf("%llu\n", unfair);
-
-  free(s);
-  free(f);
+  printf("%lld\n", min);
 
   finish=clock();
   TheTimes=(double)(finish-start)/CLOCKS_PER_SEC;
-  printf("finish : %f\n",TheTimes);
+//  printf("finish : %f\n",TheTimes);
 
   return 0;
 }
