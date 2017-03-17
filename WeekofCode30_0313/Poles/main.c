535fb5fe6a5c34a4128e97da70f4f55c78e9a3e1
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-03-17 15:30:29 +0800

    搞定,可惜超时只拿了 45

--- WeekofCode30_0313/Poles/main.c
+++ WeekofCode30_0313/Poles/main.c
@@ -9,20 +9,30 @@
 unsigned long long min;
 unsigned long long w;
 
-void proc(int * x, unsigned long long * s, unsigned long long sum, int start, int n, int k, int last_s) {
-  if (0 < k && k <= (n - start) && start < n) {
-    proc(x, s, sum, start + 1, n, k, last_s);
+long long * * p;
 
-    sum -= (x[start] - x[last_s]) * (w - s[start - 1]);
-    if (1 == k) {
-      if (sum < min) {
-        min = sum;
+long long proc(int * x, unsigned long long * s, int start, int n, int k, int last_s) {
+  long long ret = 0;
+
+  if (p[last_s][k]) {
+    ret = p[last_s][k];
+  }
+  else {
+    for (int i = start; i <= n - k; i ++) {
+      long long tmp = (x[i] - x[last_s]) * (w - s[i - 1]);
+      if (k > 1) {
+        tmp += proc(x, s, i + 1, n, k - 1, i);
+      }
+
+      if (tmp > ret) {
+        ret = tmp;
       }
     }
-    else {
-      proc(x, s, sum, start + 1, n, k - 1, start);
-    }
+
+    p[last_s][k] = ret;
   }
+
+  return ret;
 }
 
 int main(){
@@ -40,6 +50,12 @@ int main(){
     w = 0;
     unsigned long long tmp = 0;
 
+    p = malloc(sizeof(long long *) * (n + 1));
+    for (int i = 0; i < n + 1; i ++) {
+      p[i] = malloc(sizeof(long long) * (k + 1));
+      memset(p[i], 0, sizeof(long long) * (k + 1));
+    }
+
     for(int a0 = 0; a0 < n; a0++){
         int x_i;
         int w_i;
@@ -54,7 +70,9 @@ int main(){
         w += w_i;
     }
 
-    proc(x, s, min, 1, n, k - 1, 0);
+    if (0 < (k - 1)) {
+      min -= proc(x, s, 1, n, k - 1, 0);
+    }
 
     free(x);
     free(s);
