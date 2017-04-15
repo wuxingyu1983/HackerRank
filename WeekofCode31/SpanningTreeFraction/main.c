ce7207c69aa6048956fb4b0bfc41a29873343aec
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-04-15 13:08:16 +0800

    33.33 points

--- WeekofCode31/SpanningTreeFraction/main.c
+++ WeekofCode31/SpanningTreeFraction/main.c
@@ -6,70 +6,93 @@
 #include <limits.h>
 #include <stdbool.h>
 
-struct line {
-    struct point * dest;
-    int a, b;
-    struct line * nxt;
-};
+#define  E 0.000001
 
-struct point {
-    int index;
-    struct point * belong;
-    struct point * pre;
-    struct point * nxt;
-    struct line * lines;
+struct line {
+    int u, v, a, b;
+    double d;
 };
 
 int max_a, max_b;
 double max = 0;
 
-void addToSet(struct point * set, struct point * p) {
-    if (set->nxt) {
-        set->nxt->pre = p;
-    }
-    p->nxt = set->nxt;
-    p->pre = set;
-    set->nxt = p;
-    p->belong = set;
-}
-
-void delFromSet(struct point * set, struct point * p) {
-    if (p->nxt) {
-        p->nxt->pre = p->pre;
-    }
-    p->pre->nxt = p->nxt;
-}
-
-void rfunc(struct point * p, struct line * start_line, struct point * unuse, struct point * used, int a, int b) {
-//    printf("p->index = %d\n", p->index);
-    if (!unuse->nxt) {
-//        printf("%d/%d\n", a, b);
-        if ((double)a/(double)b > max) {
-            max = (double)a/(double)b;
-            max_a = a;
-            max_b = b;
-        }
+bool fequal(double a, double b) {
+    if (fabs(a - b) < E) {
+        return true;
     }
     else {
-        struct line * l = start_line;
-        if (l) {
-            do {
-                if (unuse == l->dest->belong) {
-                    struct point * p_d = l->dest;
-                    delFromSet(unuse, p_d);
-                    addToSet(used, p_d);
-
-                    rfunc(p_d, p_d->lines, unuse, used, a + l->a, b + l->b);
-                    rfunc(p, l->nxt, unuse, used, a + l->a, b + l->b);
-
-                    delFromSet(used, p_d);
-                    addToSet(unuse, p_d);
-
-                    rfunc(p, l->nxt, unuse, used, a, b);
-                }
-            } while(0 != (l = l->nxt));
-        }
+        return false;
     }
+}
+
+void quick_sort(struct line * s, int l, int r) {
+    if (l < r)
+    {
+      int i = l, j = r;
+      struct line x = s[l];
+      while (i < j)
+      {
+        while(i < j && s[j].d <= x.d)
+        j--;
+        if(i < j)
+//        s[i++] = s[j];
+        memcpy(&s[i++], &s[j], sizeof(struct line));
+
+        while(i < j && s[i].d > x.d)
+        i++;
+        if(i < j)
+//        s[j--] = s[i];
+        memcpy(&s[j--], &s[i], sizeof(struct line));
+      }
+//      s[i] = x;
+      memcpy(&s[i], &x, sizeof(struct line));
+      quick_sort(s, l, i - 1);
+      quick_sort(s, i + 1, r);
+    }
+}
+
+int find(int x, int * p) {
+    return x == p[x] ? x : (p[x] = find(p[x], p));
+}
+
+void funion(int x, int y, int * p) {
+    p[find(x, p)] = find(y, p);
+}
+
+double kruskal(int n, int m, struct line * l, double r) {
+    double ret;
+
+    // init
+    int * points = malloc(sizeof(int) * n);
+    for (size_t i = 0; i < n; i++) {
+        points[i] = i;
+    }
+    for (size_t i = 0; i < m; i++) {
+        l[i].d = (double)l[i].a - r * (double)l[i].b;
+    }
+
+    // sort
+    quick_sort(l, 0, m - 1);
+
+    max_a = 0;
+    max_b = 0;
+
+    for (int i = 0, j = 0; i < n - 1 && j < m; ++i)
+    {
+        while (find(l[j].u, points) == find(l[j].v, points)) j++;
+
+        funion(l[j].u, l[j].v, points);
+        max_a += l[j].a;
+        max_b += l[j].b;
+
+        j++;
+    }
+
+    ret = (double)max_a / (double)max_b;
+
+    free(points);
+
+    return ret;
 }
 
 int main(){
@@ -77,13 +100,8 @@ int main(){
     int m;
     scanf("%d %d",&n,&m);
 
-    struct point * * points = malloc(sizeof(struct point *) * n);
-    memset(points, 0, sizeof(struct point *) * n);
-
-    struct point * unuse = malloc(sizeof(struct point));
-    struct point * used = malloc(sizeof(struct point));
-    memset(unuse, 0, sizeof(struct point));
-    memset(used, 0, sizeof(struct point));
+    struct line * lines = malloc(sizeof(struct line) * m);
+    double start_r = 0.0;
 
     for(int a0 = 0; a0 < m; a0++){
         int u;
@@ -92,53 +110,31 @@ int main(){
         int b;
         scanf("%d %d %d %d",&u,&v,&a,&b);
         // Write Your Code Here
-        if (u == v) {
-            // loop
+
+        if (u <= v) {
+            lines[a0].u = u;
+            lines[a0].v = v;
         }
         else {
-            struct point * p_u = points[u];
-            if (!p_u) {
-                p_u = malloc(sizeof(struct point));
-                memset(p_u, 0, sizeof(struct point));
-                p_u->index = u;
-                points[u] = p_u;
-                addToSet(unuse, p_u);
-            }
+            lines[a0].u = v;
+            lines[a0].v = u;
+        }
+        lines[a0].a = a;
+        lines[a0].b = b;
 
-            struct point * p_v = points[v];
-            if (!p_v) {
-                p_v = malloc(sizeof(struct point));
-                memset(p_v, 0, sizeof(struct point));
-                p_v->index = v;
-                points[v] = p_v;
-                addToSet(unuse, p_v);
-            }
 
-            {
-                struct line * l = malloc(sizeof(struct line));
-                l->a = a;
-                l->b = b;
-                l->dest = p_v;
-                l->nxt = p_u->lines;
-                p_u->lines = l;
-            }
-
-            {
-                struct line * l = malloc(sizeof(struct line));
-                l->a = a;
-                l->b = b;
-                l->dest = p_u;
-                l->nxt = p_v->lines;
-                p_v->lines = l;
-            }
+        if (fequal(0, (double)a/(double)b) || start_r < (double)a/(double)b) {
+            start_r = (double)a/(double)b;
+            max_a = a;
+            max_b = b;
         }
     }
 
-    struct point * first = unuse->nxt;
-    delFromSet(unuse, first);
-    addToSet(used, first);
-
-    rfunc(first, first->lines, unuse, used, 0, 0);
+    double ratio = start_r;
+    do {
+        start_r = ratio;
+        ratio = kruskal(n, m, lines, start_r);
+    } while(false == fequal(ratio, start_r));
 
     int step = 2;
     while (step <= max_a && step <= max_b) {
@@ -154,7 +150,7 @@ int main(){
 
     printf("%d/%d\n", max_a, max_b);
 
-    free(points);
+    free(lines);
 
     return 0;
 }
