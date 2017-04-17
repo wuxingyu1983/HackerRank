281bda895bf3bfb2eab74ef3138e544ff34ab4e8
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-04-18 00:18:19 +0800

    wrong answer

--- SquareSubsequences/main.c
+++ SquareSubsequences/main.c
@@ -3,6 +3,49 @@
 #include <math.h>
 #include <stdlib.h>
 
+const int MOD = 1000000007;
+
+int func(char * input, int len) {
+    int ret = 0;
+
+    if (1 < len) {
+        int s[200][200];
+
+        unsigned long long cnt = 0;
+        // init
+        for (size_t i = 0; i < 200; i++) {
+            memset(s[i], 0, sizeof(int) * 200);
+        }
+
+        for (size_t i = 1; i < len; i++) {
+            for (size_t j = 0; j < i; j++) {
+                if (input[j] == input[i]) {
+                    s[j][i] = 1;
+                    if (0 < j && j < i - 1) {
+                        for (size_t k = 0; k < j; k ++) {
+                            for (size_t l = j + 1; l < i; l ++) {
+                                if (s[k][l]) {
+                                    s[j][i] += s[k][l];
+                                    s[j][i] %= MOD;
+                                }
+                            }
+                        }
+                    }
+
+                    printf("s[%d][%d] = %d\n", (int)j, (int)i, s[j][i]);
+
+                    cnt += s[j][i];
+                    cnt %= MOD;
+                }
+            }
+
+        }
+        ret = cnt;
+    }
+
+    return ret;
+}
+
 int main() {
     int t;
     scanf("%d", &t);
@@ -15,7 +58,7 @@ int main() {
 
         int len = strlen(s);
 
-        
+        output[i] = func(s, len);
     }
 
     for (int i = 0; i < t; i ++) {
