afb1b7e6a3dadc46f278a431126d7b81ac641d45
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-04-23 21:30:59 +0800

    50 points

--- InsertionSortAdvancedAnalysis/main.c
+++ InsertionSortAdvancedAnalysis/main.c
@@ -3,70 +3,66 @@
 #include <math.h>
 #include <stdlib.h>
 
-unsigned long long getMoves(int * a, int len) {
+unsigned long long merge_sort(int * a, int s, int e) {
     unsigned long long ret = 0;
-    int * s = malloc(sizeof(int) * len * 2);
-    memset(s, 0, sizeof(int) * len * 2);
 
-    int start = -1, end = -1;
+    if (s < e) {
+        int middle = (s + e) / 2;
 
-    for (size_t i = 0; i < len; i++) {
-        if (0 > start) {
-            start = len;
-            s[start] = a[i];
-            end = len;
-        }
-        else {
-            int add = 0;
-            // 二分法找到位置
-            int l, r, m;
-            l = start;
-            r = end;
-            while (l <= r) {
-                m = (l + r) / 2;
-                if (a[i] == s[m]) {
-                    break;
-                }
-                else if (a[i] < s[m]) {
-                    r = m - 1;
+        ret += merge_sort(a, s, middle);
+        ret += merge_sort(a, middle + 1, e);
+
+        int * t = malloc(sizeof(int) * (e - s + 1));
+        memset(t, 0 , sizeof(int) * (e - s + 1));
+
+        int i = s, j = middle + 1;
+        for (size_t k = s; k <= e; k++) {
+            if (i <= middle && j <= e) {
+                if (a[i] <= a[j]) {
+                    t[k - s] = a[i];
+                    if (k > i) {
+                        ret += k - i;
+                    }
+                    i ++;
                 }
                 else {
-                    l = m + 1;
+                    t[k - s] = a[j];
+                    if (k > j) {
+                        ret += k - j;
+                    }
+                    j ++;
                 }
-            }
-
-            if (s[m] == a[i]) {
-                while (s[m] == a[i]) {
-                    m ++;
-                }
-            }
-            else if (s[m] > a[i]) {
             }
             else {
-                m ++;
-            }
-
-            if (end - m + 1 <= m - start) {
-                for (size_t j = end + 1; j > m; j --) {
-                    s[j] = s[j - 1];
+                if (i <= middle) {
+                    t[k - s] = a[i];
+                    if (k > i) {
+                        ret += k - i;
+                    }
+                    i ++;
                 }
-                s[m] = a[i];
-                add = end - m + 1;
-                end ++;
-            }
-            else {
-                for (size_t j = start; j < m; j ++) {
-                    s[j - 1] = s[j];
+                else {
+                    t[k - s] = a[j];
+                    if (k > j) {
+                        ret += k - j;
+                    }
+                    j ++;
                 }
-                s[m - 1] = a[i];
-                add = end - m + 1;
-                start --;
             }
-            ret += add;
         }
+
+        memcpy(&a[s], t, sizeof(int) * (e - s + 1));
+
+        free(t);
     }
 
-    free(s);
+    return ret;
+}
+
+unsigned long long getMoves(int * a, int len) {
+    unsigned long long ret = 0;
+
+    ret = merge_sort(a, 0, len - 1);
 
     return ret;
 }
