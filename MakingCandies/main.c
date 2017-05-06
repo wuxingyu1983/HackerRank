55ebe4bbe828f0a3d7f4f45550f891ecf196b4af
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-05-06 16:56:01 +0800

    24.44 points

--- MakingCandies/main.c
+++ MakingCandies/main.c
@@ -12,45 +12,50 @@ unsigned long long makingCandy( unsigned long long m,
         unsigned long long ret = 0;
 
         if (n > sum) {
-            // 按照当前速率
-            if (0 == (n - sum) % (m * w)) {
-                ret = (n - sum) / (m * w);
+            if (m > (n - sum) / w) {
+                ret = 1;
             }
             else {
-                ret = (n - sum) / (m * w) + 1;
-            }
-
-            unsigned long long added = 1;
-            while (added <= (n - sum) / p + 1) {
-                unsigned long long days = 0;
-                if (0 == (p * added) % (m * w)) {
-                    days = (p * added) / (m * w);
+                // 按照当前速率
+                if (0 == (n - sum) % (m * w)) {
+                    ret = (n - sum) / (m * w);
                 }
                 else {
-                    days = (p * added) / (m * w) + 1;
+                    ret = (n - sum) / (m * w) + 1;
                 }
 
-                if (days >= ret) {
-                    break;
-                }
-
-                if (0 == added % 2){
-                    days += makingCandy(m + added / 2, w + added / 2, p, sum + days * (m * w) - added * p, n);
-                }
-                else {
-                    if (m > w) {
-                        days += makingCandy(m + added / 2, w + added / 2 + 1, p, sum + days * (m * w) - added * p, n);
+                unsigned long long added = 1;
+                while (added <= (n - sum) / p + 1) {
+                    unsigned long long days = 0;
+                    if (0 == (p * added) % (m * w)) {
+                        days = (p * added) / (m * w);
                     }
                     else {
-                        days += makingCandy(m + added / 2 + 1, w + added / 2, p, sum + days * (m * w) - added * p, n);
+                        days = (p * added) / (m * w) + 1;
                     }
-                }
 
-                if (days < ret) {
-                    ret = days;
-                }
+                    if (days >= ret) {
+                        break;
+                    }
 
-                added ++;
+                    if (0 == added % 2){
+                        days += makingCandy(m + added / 2, w + added / 2, p, sum + days * (m * w) - added * p, n);
+                    }
+                    else {
+                        if (m > w) {
+                            days += makingCandy(m + added / 2, w + added / 2 + 1, p, sum + days * (m * w) - added * p, n);
+                        }
+                        else {
+                            days += makingCandy(m + added / 2 + 1, w + added / 2, p, sum + days * (m * w) - added * p, n);
+                        }
+                    }
+
+                    if (days < ret) {
+                        ret = days;
+                    }
+
+                    added ++;
+                }
             }
         }
 
