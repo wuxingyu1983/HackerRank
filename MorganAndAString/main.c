37bab52f9bbcb7d4a1c83a08f7739c96a4b8c03c
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-06-10 17:48:11 +0800

    调整代码

--- MorganAndAString/main.c
+++ MorganAndAString/main.c
@@ -3,25 +3,39 @@
 #include <math.h>
 #include <stdlib.h>
 
-char getNextDiff(char s[], char letter) {
+#define DEBUG       1
+
+char getNextDiff(char * s, char letter) {
     char ret = letter;
 
     int i = 0;
+//    printf("s[0] is %c\n", s[i]);
     while (s[i]) {
         if (s[i] != letter) {
-            ret = letter;
+            ret = s[i];
             break;
         }
 
         i ++;
     }
 
+//    printf("the next diff from %c is %c\n", letter, ret);
+
     return ret;
 }
 
 int main() {
+#if DEBUG
+    FILE * fp = fopen("input.txt", "r");
+#endif
+
     int t;
+
+#if DEBUG
+    fscanf(fp, "%d", &t);
+#else
     scanf("%d", &t);
+#endif
 
     char a[5][100001];
     char b[5][100001];
@@ -30,8 +44,13 @@ int main() {
         memset(a[i], 0, 100001);
         memset(b[i], 0, 100001);
 
+#if DEBUG
+        fscanf(fp, "%s", a[i]);
+        fscanf(fp, "%s", b[i]);
+#else
         scanf("%s", a[i]);
         scanf("%s", b[i]);
+#endif
     }
 
     for (size_t i = 0; i < t; i++) {
@@ -39,8 +58,8 @@ int main() {
         while (a[i][a_i] || b[i][b_i]) {
             if (a[i][a_i] && b[i][b_i]) {
                 if (a[i][a_i] == b[i][b_i]) {
-                    char next_a = getNextDiff(&a[i][a_i + 1], a[i][a_i]);
-                    char next_b = getNextDiff(&b[i][b_i + 1], b[i][b_i]);
+                    char next_a = getNextDiff(&(a[i][a_i + 1]), a[i][a_i]);
+                    char next_b = getNextDiff(&(b[i][b_i + 1]), b[i][b_i]);
 
                     if (next_a < next_b) {
                         printf("%c", a[i][a_i]);
@@ -74,6 +93,10 @@ int main() {
 
         printf("\n");
     }
+
+#if DEBUG
+    fclose(fp);
+#endif
 
     return 0;
 }
