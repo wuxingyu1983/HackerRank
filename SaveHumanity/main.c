995e6eb58a2b3348b81a36351e22e57fc29fbe42
wuxingyu <wuxingyu1983@gmail.com>
2017-08-05 19:00:43 +0800

    44.44 points

--- SaveHumanity/main.c
+++ SaveHumanity/main.c
@@ -28,25 +28,19 @@ void getFailure(char v[], int f[])
 	}
 }
 
-int save_humanity(char pat[], char vir[], int out[], int f[]) {
-	int ret = 0;
-	int len_p = strlen(pat);
-	int len_v = strlen(vir);
+int can_match(char v, char p, int pos_v, int pos_p, int pos_dismatch_in_pat) {
+	int ret = 1;
 
-	if (len_p >= len_v) {
-		getFailure(vir, f);
-
-		for (int i=0, j=-1; i<len_p; ++i)
-		{
-			while (j >= 0 && vir[j+1] != pat[i])
-				j = f[j];
-
-			if (vir[j+1] == pat[i]) j++;
-
-			if (j == len_v-1)
-			{
-				out[ret ++] = i - len_v + 1;
-				j = f[j];
+	if (v != p) {
+		if (pos_dismatch_in_pat < 0) {
+		}
+		else {
+			if (pos_p - pos_dismatch_in_pat > pos_v) {
+				// can
+			}
+			else {
+				// can not
+				ret = 0;
 			}
 		}
 	}
@@ -54,45 +48,100 @@ int save_humanity(char pat[], char vir[], int out[], int f[]) {
 	return ret;
 }
 
-int main() {
-	int t;
+int save_humanity(char pat[], char vir[], int out[], int f[]) {
+	int ret = 0;
+	int len_p = strlen(pat);
+	int len_v = strlen(vir);
+	int dismatch = -1;
 
-#if DEBUG
-	FILE * fp = fopen("input.txt", "r");
-	fscanf(fp, "%d", &t);
-	FILE * fp_out = fopen("output.txt", "w");
-#else
-	scanf("%d", &t);
-#endif
+	if (len_p >= len_v) {
+		getFailure(vir, f);
 
-	for (size_t i = 0; i < t; i++) {
-#if DEBUG
-		fscanf(fp, "%s %s", p[i], v[i]);
-#else
-		scanf("%s %s", p[i], v[i]);
-#endif
-		cnt[i] = save_humanity(p[i], v[i], output[i], failure[i]);
-	}
+		int start = -1;
 
-	for (size_t i = 0; i < t; i++) {
-		if (0 >= cnt[i]) {
-			printf("No Match!\n");
-		}
-		else {
-			int j = 0;
-			printf("%d", output[i][j++]);
-			while (j < cnt[i]) {
-				printf(" %d", output[i][j++]);
+		for (int i=0, j=-1; i<len_p; ++i)
+		{
+			while (j >= 0 && !can_match(vir[j+1], pat[i], j+1, i, dismatch)) {
+				//			while (j >= 0 && vir[j+1] != pat[i]) {
+				if (-1 == f[j]) {
+					i -= j;
+					j = -1;
+					dismatch = -1;
+				}
+				else {
+					j = f[j];
+				}
 			}
-			printf("\n");
+
+			int exact_match = 0;
+			if (can_match(vir[j+1], pat[i], j+1, i, dismatch)) {
+				if (vir[j+1] == pat[i]) {
+					exact_match = 1;
+				}
+				else {
+					exact_match = 2;
+				}
+				j++;
+			}
+			//			if (vir[j+1] == pat[i]) j++;
+
+			if (j == len_v-1)
+			{
+				out[ret ++] = i - len_v + 1;
+				i = i - len_v + 1;
+				j = -1;
+				dismatch = -1;
+				exact_match = 0;
+			}
+
+			if (2 == exact_match) {
+				dismatch = i;
+			}
+			}
 		}
+
+		return ret;
 	}
 
+	int main() {
+		int t;
+
 #if DEBUG
-	fclose(fp);
-	fclose(fp_out);
+		FILE * fp = fopen("input.txt", "r");
+		fscanf(fp, "%d", &t);
+		FILE * fp_out = fopen("output.txt", "w");
+#else
+		scanf("%d", &t);
 #endif
 
-	return 0;
-}
+		for (size_t i = 0; i < t; i++) {
+#if DEBUG
+			fscanf(fp, "%s %s", p[i], v[i]);
+#else
+			scanf("%s %s", p[i], v[i]);
+#endif
+			cnt[i] = save_humanity(p[i], v[i], output[i], failure[i]);
+		}
+
+		for (size_t i = 0; i < t; i++) {
+			if (0 >= cnt[i]) {
+				printf("No Match!\n");
+			}
+			else {
+				int j = 0;
+				printf("%d", output[i][j++]);
+				while (j < cnt[i]) {
+					printf(" %d", output[i][j++]);
+				}
+				printf("\n");
+			}
+		}
+
+#if DEBUG
+		fclose(fp);
+		fclose(fp_out);
+#endif
+
+		return 0;
+	}
 
