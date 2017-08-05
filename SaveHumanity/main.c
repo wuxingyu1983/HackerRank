9e3540656779cdb4d3160ede3bc4fbd4c3572939
wuxingyu <wuxingyu1983@gmail.com>
2017-08-05 14:30:55 +0800

    先实现完全匹配的。

--- SaveHumanity/main.c
+++ SaveHumanity/main.c
@@ -12,56 +12,87 @@ char v[10][100001];
 int output[10][100000];
 int cnt[10];
 
-int save_humanity(char pat[], char vir[], int out[]) {
-    int ret = 0;
-    int len_p = strlen(pat);
-    int len_v = strlen(vir);
+int failure[10][100000];
 
-    if (len_p >= len_v) {
-        
-    }
+void getFailure(char v[], int f[])
+{
+	int len = strlen(v);
+	for (int i=1, j=f[0]=-1; i<len; ++i)
+	{
+		while (j >= 0 && v[j+1] != v[i])
+			j = f[j];
 
-    return ret;
+		if (v[j+1] == v[i]) j++;
+
+		f[i] = j;
+	}
+}
+
+int save_humanity(char pat[], char vir[], int out[], int f[]) {
+	int ret = 0;
+	int len_p = strlen(pat);
+	int len_v = strlen(vir);
+
+	if (len_p >= len_v) {
+		getFailure(vir, f);
+
+		for (int i=0, j=-1; i<len_p; ++i)
+		{
+			while (j >= 0 && vir[j+1] != pat[i])
+				j = f[j];
+
+			if (vir[j+1] == pat[i]) j++;
+
+			if (j == len_v-1)
+			{
+				out[ret ++] = i - len_v + 1;
+				j = f[j];
+			}
+		}
+	}
+
+	return ret;
 }
 
 int main() {
-    int t;
+	int t;
 
 #if DEBUG
-    FILE * fp = fopen("input.txt", "r");
-    fscanf(fp, "%d", &t);
-    FILE * fp_out = fopen("output.txt", "w");
+	FILE * fp = fopen("input.txt", "r");
+	fscanf(fp, "%d", &t);
+	FILE * fp_out = fopen("output.txt", "w");
 #else
-    scanf("%d", &t);
+	scanf("%d", &t);
 #endif
 
-    for (size_t i = 0; i < t; i++) {
+	for (size_t i = 0; i < t; i++) {
 #if DEBUG
-        fscanf(fp, "%s %s", p[i], v[i]);
+		fscanf(fp, "%s %s", p[i], v[i]);
 #else
-        scanf("%s %s", p[i], v[i]);
+		scanf("%s %s", p[i], v[i]);
 #endif
-        cnt[i] = save_humanity(p[i], v[i], output[i]);
-    }
+		cnt[i] = save_humanity(p[i], v[i], output[i], failure[i]);
+	}
 
-    for (size_t i = 0; i < t; i++) {
-        if (0 >= cnt[i]) {
-            printf("No Match!\n");
-        }
-        else {
-            int j = 0;
-            printf("%d", output[i][j++]);
-            while (j < cnt[i]) {
-                printf(" %d", output[i][j++]);
-            }
-            printf("\n");
-        }
-    }
+	for (size_t i = 0; i < t; i++) {
+		if (0 >= cnt[i]) {
+			printf("No Match!\n");
+		}
+		else {
+			int j = 0;
+			printf("%d", output[i][j++]);
+			while (j < cnt[i]) {
+				printf(" %d", output[i][j++]);
+			}
+			printf("\n");
+		}
+	}
 
 #if DEBUG
-    fclose(fp);
-    fclose(fp_out);
+	fclose(fp);
+	fclose(fp_out);
 #endif
 
-    return 0;
+	return 0;
 }
+
