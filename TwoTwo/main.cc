2711970ab16fd801833b912cf308e1411621d972
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-09-13 11:51:08 +0800

    计算所有2的幂

--- TwoTwo/main.cc
+++ TwoTwo/main.cc
@@ -15,11 +15,36 @@ using namespace std;
 #define MAX_2       801
 #define MAX_S       100001
 
-int output[MAX_T];
+int count[MAX_T];
+
+int len_two[MAX_2];
+vector<string> str_two(MAX_2);
 
 void init() {
+    for (size_t i = 0; i < MAX_2; i++) {
+        if (0 == i) {
+            str_two[i] = '1';
+        }
+        else {
+            int carry = 0;
+            int val = 0;
+            for (size_t j = 0; j < str_two[i - 1].length(); j++) {
+                val = (str_two[i - 1][j] - '0') * 2 + carry;
+                if (9 < val) {
+                    str_two[i] += '0' + val - 10;
+                    carry = 1;
+                }
+                else {
+                    str_two[i] += '0' + val;
+                    carry = 0;
+                }
+            }
 
-
+            if (0 < carry) {
+                str_two[i] += '1';
+            }
+        }
+    }
 }
 
 int two_two(string& s) {
@@ -52,11 +77,11 @@ int main() {
 #else
         cin >> s;
 #endif
-        output[i] = two_two(s);
+        count[i] = two_two(s);
     }
 
     for (size_t i = 0; i < t; i++) {
-        cout << output[i];
+        cout << count[i];
     }
 
     return 0;
