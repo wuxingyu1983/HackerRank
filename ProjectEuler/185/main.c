fba6c8c5af0a311bcd0b7861f425e9bcd6c8bf2d
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-11-09 16:21:20 +0800

    50.00 points

--- ProjectEuler/185/main.c
+++ ProjectEuler/185/main.c
@@ -5,10 +5,38 @@
 #include <stdbool.h>
 
 #define MAX_LEN     12
+#define MAX_N       30
 
-char s[30][MAX_LEN + 1];
+char s[MAX_N][MAX_LEN + 1];
 char output[MAX_LEN + 1];
-int c[30][MAX_LEN + 1];
+int c[MAX_N][MAX_LEN + 1];
+
+#define SEED	   131
+#define MOD		   10000007
+
+long long _pow[MAX_N];
+
+char flag[MAX_LEN][MOD];    // 0 - undefine, 1 - false, 2 - true (nerver happen)
+
+void init_pow(long long p[], long long seed, long long mod) {
+    p[0] = 1;
+    for (size_t i = 1; i < MAX_N; i++) {
+        p[i] = (p[i - 1] * seed) % mod;
+    }
+
+    return;
+}
+
+int get_hash(int n, int index) {
+    long long ret = 0;
+
+    for (size_t j = 0; j < n; j++) {
+        ret += _pow[j] * (long long)c[j][index] % MOD;
+        ret = ret % MOD;
+    }
+
+    return (int)ret;
+}
 
 bool func(int n, int index) {
     bool ret = true;
@@ -46,9 +74,20 @@ bool func(int n, int index) {
         if (ret) {
             output[index] = i + '0';
             if ((MAX_LEN - 1) > index) {
-                ret = func(n, index + 1);
-                if (ret) {
-                    break;
+                int hash = get_hash(n, index + 1);
+                if (0 == flag[index + 1][hash]) {
+                    ret = func(n, index + 1);
+                    if (ret) {
+                        break;
+                    }
+                    else {
+                        // mark flag
+                        flag[index + 1][hash] = 1;
+                    }
+                }
+                else {
+                    // 1 - false
+                    ret = false;
                 }
             }
             else {
@@ -56,6 +95,12 @@ bool func(int n, int index) {
                 break;
             }
         }
+    }
+
+    if (false == ret) {
+        // mark flag
+        int hash = get_hash(n, index);
+        flag[index][hash] = 1;
     }
 
     return ret;
@@ -68,6 +113,8 @@ int main() {
     for (size_t i = 0; i < n; i++) {
         scanf("%s %d", s[i], &c[i][0]);
     }
+
+    init_pow(_pow, SEED, MOD);
 
     func(n, 0);
 
