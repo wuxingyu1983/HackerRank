81b8974eab9e23f4fd8a6a18e64714a9e5094201
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2017-11-11 22:57:58 +0800

    添加2 correct 的处理

--- ProjectEuler/185/main.c
+++ ProjectEuler/185/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_LEN     12
+#define MAX_LEN     5
 #define MAX_N       30
 
 char s[MAX_N][MAX_LEN + 1];
@@ -75,16 +75,18 @@ bool func(int n, int index) {
                     impossible[pos][s[index][pos] - '0'] --;
                     output[pos] = s[index][pos] - '0';
 
+                    ret = true;
                     if (index < n - 1) {
                         ret = func(n, index + 1);
-                        if (ret) {
-                            break;
-                        }
-                        else {
+                        if (false == ret) {
                             // mark impossible
                             impossible[pos][s[index][pos] - '0'] ++;
                             output[pos] = -1;
                         }
+                    }
+
+                    if (ret) {
+                        break;
                     }
                 }
             }
@@ -98,7 +100,132 @@ bool func(int n, int index) {
         }
     }
     else if (2 == c[index]) {
+        int already = 0;
+        for (size_t i = 0; i < MAX_LEN; i++) {
+            if (output[i] == (s[index][i] - '0')) {
+                already ++;
+            }
+        }
+        if (2 < already) {
+            ret = false;
+        }
+        else if (2 == already) {
+            // mark impossible
+            for (size_t i = 0; i < MAX_LEN; i++) {
+                if (output[i] != (s[index][i] - '0')) {
+                    impossible[i][s[index][i] - '0'] ++;
+                }
+            }
 
+            if (index < n - 1) {
+                ret = func(n, index + 1);
+                if (false == ret) {
+                    // unmark impossible
+                    for (size_t i = 0; i < MAX_LEN; i++) {
+                        if (output[i] != (s[index][i] - '0')) {
+                            impossible[i][s[index][i] - '0'] --;
+                        }
+                    }
+                }
+            }
+        }
+        else if (1 == already) {
+            // mark impossible
+            for (size_t i = 0; i < MAX_LEN; i++) {
+                if (output[i] != (s[index][i] - '0')) {
+                    impossible[i][s[index][i] - '0'] ++;
+                }
+            }
+
+            ret = false;
+            for (size_t pos = 0; pos < MAX_LEN; pos++) {
+                if (1 == impossible[pos][s[index][pos] - '0'] && 0 > output[pos]) {
+                    ret = true;
+                    // unmark impossible
+                    impossible[pos][s[index][pos] - '0'] --;
+                    output[pos] = s[index][pos] - '0';
+
+                    if (index < n - 1) {
+                        ret = func(n, index + 1);
+                        if (false == ret) {
+                            // mark impossible
+                            impossible[pos][s[index][pos] - '0'] ++;
+                            output[pos] = -1;
+                        }
+                    }
+
+                    if (ret) {
+                        break;
+                    }
+                }
+            }
+
+            if (false == ret) {
+                // unmark impossible
+                for (size_t i = 0; i < MAX_LEN; i++) {
+                    if (output[i] != (s[index][i] - '0')) {
+                        impossible[i][s[index][i] - '0'] --;
+                    }
+                }
+            }
+        }
+        else {
+            // 0 == already
+            // mark impossible
+            for (size_t i = 0; i < MAX_LEN; i++) {
+                if (output[i] != (s[index][i] - '0')) {
+                    impossible[i][s[index][i] - '0'] ++;
+                }
+            }
+
+            ret = false;
+            for (size_t pos = 0; pos < MAX_LEN - 1; pos++) {
+                if (1 == impossible[pos][s[index][pos] - '0'] && 0 > output[pos]) {
+                    // unmark impossible
+                    impossible[pos][s[index][pos] - '0'] --;
+                    output[pos] = s[index][pos] - '0';
+
+                    for (size_t pos1 = pos + 1; pos1 < MAX_LEN; pos1++) {
+                        if (1 == impossible[pos1][s[index][pos1] - '0'] && 0 > output[pos1]) {
+                            // unmark impossible
+                            impossible[pos1][s[index][pos1] - '0'] --;
+                            output[pos1] = s[index][pos1] - '0';
+
+                            ret = true;
+                            if (index < n - 1) {
+                                ret = func(n, index + 1);
+                                if (false == ret) {
+                                    // mark impossible
+                                    impossible[pos1][s[index][pos1] - '0'] ++;
+                                    output[pos1] = -1;
+                                }
+                            }
+
+                            if (ret) {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (ret) {
+                        break;
+                    }
+                    else {
+                        impossible[pos][s[index][pos] - '0'] ++;
+                        output[pos] = -1;
+                    }
+                }
+            }
+
+            if (false == ret) {
+                // unmark impossible
+                for (size_t i = 0; i < MAX_LEN; i++) {
+                    if (output[i] != (s[index][i] - '0')) {
+                        impossible[i][s[index][i] - '0'] --;
+                    }
+                }
+            }
+        }
     }
     else {
         // 3 == c[index]
