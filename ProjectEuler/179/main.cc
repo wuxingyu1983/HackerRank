5b3cfe2c55a9e1a906533fa38fa5384c0ebfbfbc
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-11-24 08:26:28 +0800

    init

---
+++ ProjectEuler/179/main.cc
@@ -0,0 +1,38 @@
+#include <string>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+
+#define MAX_K          10000000
+#define MAX_T          1000000
+
+using namespace std;
+
+int cnt[MAX_K + 1];
+
+void init() {
+    
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    init();
+    vector<int> output;
+
+    for (size_t i = 0; i < output.size(); i++) {
+        cout << output[i] << endl;
+    }
+
+    return 0;
+}
