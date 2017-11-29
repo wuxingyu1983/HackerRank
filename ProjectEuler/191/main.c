eb6b37b0a0f03f46b49f1ad3abfa1a138c63b09d
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2017-11-29 17:31:11 +0800

    27.45 points

--- ProjectEuler/191/main.c
+++ ProjectEuler/191/main.c
@@ -27,40 +27,43 @@ int main() {
         memset(cnt2[i], 0xff, m * sizeof(long long));
     }
 
+    long long * sum_n_1 = (long long *)malloc((n + 1) * sizeof(long long));
+    memset(sum_n_1, 0, (n + 1) * sizeof(long long));
+
+    long long * sum_n_2 = (long long *)malloc((n + 1) * sizeof(long long));
+    memset(sum_n_2, 0, (n + 1) * sizeof(long long));
+
     long long sum = 0;
 
     cnt1[0][0] = (c - 2) % MOD;
     cnt1[0][1] = 1;
     cnt1[1][0] = 1;
 
+    sum_n_1[0] = (c - 1) % MOD;
+    sum_n_1[1] = 1;
+
     sum += ((long long)(c % MOD)) * (long long)n % MOD;
 
     long long * * now = cnt2;
     long long * * pre = cnt1;
 
+    long long * now_sum = sum_n_2;
+    long long * pre_sum = sum_n_1;
+
     for (size_t day = 2; day <= l; day++) {
         int max_n = (n < day) ? n : day;
         for (size_t in = 0; in <= max_n; in++) {
             long long tmp = 0;
+            long long sum_tmp = 0;
             int max_m = ((m - 1) < (day - in)) ? (m - 1) : (day - in);
             for (size_t im = 0; im <= max_m; im++) {
                 if (0 == im) {
-                    for (size_t i = 0; i < m; i++) {
-                        if (0 > pre[in][i]) {
-                            break;
-                        }
-                        tmp += pre[in][i] * (long long)((c - 2) % MOD);
-                        tmp %= MOD;
-                    }
+                    tmp += pre_sum[in] * (long long)((c - 2) % MOD);
+                    tmp %= MOD;
 
                     if (in > 0) {
-                        for (size_t i = 0; i < m; i++) {
-                            if (0 > pre[in - 1][i]) {
-                                break;
-                            }
-                            tmp += pre[in - 1][i];
-                            tmp %= MOD;
-                        }
+                        tmp += pre_sum[in - 1];
+                        tmp %= MOD;
                     }
 
                     now[in][im] = tmp;
@@ -69,6 +72,9 @@ int main() {
                     now[in][im] = pre[in][im - 1];
                 }
 //                printf("day = %zu, now[%zu][%zu] = %lld\n", day, in, im, now[in][im]);
+
+                sum_tmp += now[in][im];
+                sum_tmp %= MOD;
 
                 if (0 == in) {
                     sum += (long long)n * (long long)now[in][im];
@@ -79,6 +85,8 @@ int main() {
                     sum %= MOD;
                 }
             }
+
+            now_sum[in] = sum_tmp;
         }
 
         if (now == cnt2) {
@@ -88,6 +96,15 @@ int main() {
         else {
             now = cnt2;
             pre = cnt1;
+        }
+
+        if (now_sum == sum_n_1) {
+            now_sum = sum_n_2;
+            pre_sum = sum_n_1;
+        }
+        else {
+            now_sum = sum_n_1;
+            pre_sum = sum_n_2;
         }
     }
 
