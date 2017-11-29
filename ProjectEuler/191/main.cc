51e74f098a29b01e4e73cc43d82e5478fdf9fb24
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2017-11-29 14:28:40 +0800

    init

---
+++ ProjectEuler/191/main.cc
@@ -0,0 +1,94 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MOD         1000000007
+
+int * * cnt1;
+int * * cnt2;
+
+int main() {
+    int l, n, m;
+    long long c;
+
+    scanf("%d %d %d %lld", &l, &n, &m, &c);
+
+    // malloc
+    cnt1 = (int * *)malloc((n + 1) * sizeof(int*));
+    for (size_t i = 0; i <= n; i++) {
+        cnt1[i] = (int *)malloc(m * sizeof(int));
+        memset(cnt1[i], 0, m * sizeof(int));
+    }
+
+    cnt2 = (int * *)malloc((n + 1) * sizeof(int*));
+    for (size_t i = 0; i <= n; i++) {
+        cnt2[i] = (int *)malloc(m * sizeof(int));
+        memset(cnt2[i], 0, m * sizeof(int));
+    }
+
+    long long sum = 0;
+
+    cnt1[0][0] = (c - 2) % MOD;
+    cnt1[0][1] = 1;
+    cnt1[1][0] = 1;
+
+    sum += ((long long)(c % MOD)) * (long long)n % MOD;
+
+    printf("sum is %lld\n", sum);
+
+    int * * now = cnt2;
+    int * * pre = cnt1;
+
+    for (size_t day = 2; day <= l; day++) {
+        int max_n = (n < day) ? n : day;
+        for (size_t in = 0; in <= max_n; in++) {
+            long long tmp = 0;
+            int max_m = ((m - 1) < (day - in)) ? (m - 1) : (day - in);
+            for (size_t im = 0; im <= max_m; im++) {
+                if (0 == im) {
+                    for (size_t i = 0; i < m; i++) {
+                        tmp += (c - 2) * pre[in][i] % MOD;
+                        tmp %= MOD;
+                    }
+
+                    if (in > 0) {
+                        for (size_t i = 0; i < m; i++) {
+                            tmp += pre[in - 1][i];
+                            tmp %= MOD;
+                        }
+                    }
+
+                    now[in][im] = tmp;
+                }
+                else {
+                    now[in][im] = pre[in][im - 1];
+                }
+
+                if (0 == in) {
+                    sum += (long long)l * (long long)now[in][im];
+                    sum %= MOD;
+                }
+                else {
+                    sum += (long long)(l - in + 1) * (long long)now[in][im];
+                    sum %= MOD;
+                }
+            }
+        }
+
+        if (now == cnt2) {
+            now = cnt1;
+            pre = cnt2;
+        }
+        else {
+            now = cnt2;
+            pre = cnt1;
+        }
+    }
+
+    printf("%lld\n", sum);
+
+    return 0;
+}
