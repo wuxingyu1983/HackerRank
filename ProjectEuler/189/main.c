ef1afd2aabbbfe2188335c4b8350891a96830088
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-12-06 23:09:23 +0800

    init

---
+++ ProjectEuler/189/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MOD         1000000007
+
+int main() {
+    long long ret = 0;
+
+    int n, c;
+    scanf("%d %d", &n, &c);
+
+    unsigned char bits[] = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4};
+
+    printf("%lld\n", ret);
+
+    return 0;
+}
