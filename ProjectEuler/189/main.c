51449986930c674e2019a6028b56a4f3e8743f78
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2017-12-11 17:57:50 +0800

    13.33 points

--- ProjectEuler/189/main.c
+++ ProjectEuler/189/main.c
@@ -1,9 +1,60 @@
+//
+//  main.c
+//  189
+//
+//  Created by 吴星煜 on 2017/12/11.
+//  Copyright © 2017年 吴星煜. All rights reserved.
+//
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MOD         1000000007
+
+unsigned char bits[] = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4};
+
+long long getIndex(int level[], int c) {
+    long long ret = 0;
+
+    int i = 0;
+    while (0 <= level[i]) {
+        ret = ret << bits[c];
+        ret += level[i];
+
+        i ++;
+    }
+
+    return ret;
+}
+
+long long getMultiplier(int pre_level[], int now_level[], int c) {
+    long long ret = 1;
+
+    int i = 0;
+    while (0 <= pre_level[i]) {
+        int same = 0;
+        if (pre_level[i] == now_level[i]) {
+            same ++;
+        }
+        if (pre_level[i] == now_level[i + 1]) {
+            same ++;
+        }
+
+        if (0 == (c - 3 + same)) {
+            return 0;
+        }
+        else {
+            ret *= (long long)(c - 3 + same);
+        }
+
+        i ++;
+    }
+
+    return ret;
+}
 
 int main() {
     long long ret = 0;
@@ -11,7 +62,204 @@ int main() {
     int n, c;
     scanf("%d %d", &n, &c);
 
-    unsigned char bits[] = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4};
+    int arry_size = pow(2, bits[c] * ((n - 1) ? (n - 1) : 1));
+
+    long long * tmp1 = (long long *)malloc(arry_size * sizeof(long long));
+    long long * tmp2 = (long long *)malloc(arry_size * sizeof(long long));
+    memset(tmp1, 0, arry_size * sizeof(long long));
+    memset(tmp2, 0, arry_size * sizeof(long long));
+
+    for (size_t i = 0; i < c; i++) {
+        tmp1[i] = 1;
+    }
+    ret = c;
+
+    if (n > 1) {
+        long long * now = tmp2;
+        long long * pre = tmp1;
+
+        int now_level[14];
+        int pre_level[14];
+        for (size_t j = 2; j <= n - 1; j++) {
+            if (0 < j - 2) {
+                // clean array tmp
+                arry_size = pow(2, (j - 2) * bits[c]);
+                memset(now, 0, arry_size * sizeof(int));
+            }
+
+            for (size_t i = 0; i < j; i++) {
+                now_level[i] = 0;
+            }
+            for (size_t i = j; i < 14; i++) {
+                now_level[i] = -1;
+            }
+
+            while (true) {
+                for (size_t i = 0; i < j - 1; i++) {
+                    pre_level[i] = 0;
+                }
+                for (size_t i = j - 1; i < 14; i++) {
+                    pre_level[i] = -1;
+                }
+                int now_index = getIndex(now_level, c);
+
+                while (true) {
+                    // proc
+                    int pre_index = getIndex(pre_level, c);
+
+                    if (pre[pre_index]) {
+                        long long multiplier = getMultiplier(pre_level, now_level, c);
+
+                        if (0 < multiplier) {
+                            long long product = pre[pre_index];
+                            product *= multiplier;
+                            product %= MOD;
+
+                            now[now_index] += product;
+                            now[now_index] %= MOD;
+                        }
+                    }
+
+                    // 遍历 pre_level
+                    if (c - 1 == pre_level[j - 2]) {
+                        int pos = j - 2;
+                        while (c - 1 == pre_level[pos] && pos >= 0) {
+                            pos --;
+                        }
+
+                        if (0 > pos) {
+                            break;
+                        }
+                        else {
+                            pre_level[pos] ++;
+                            for (size_t i = pos + 1; i < j - 1; i++) {
+                                pre_level[i] = 0;
+                            }
+                        }
+                    }
+                    else {
+                        pre_level[j - 2] ++;
+                    }
+                }
+
+                // 遍历 now_level
+                if (c - 1 == now_level[j - 1]) {
+                    int pos = j - 1;
+                    while (c - 1 == now_level[pos] && pos >= 0) {
+                        pos --;
+                    }
+
+                    if (0 > pos) {
+                        break;
+                    }
+                    else {
+                        now_level[pos] ++;
+                        for (size_t i = pos + 1; i < j; i++) {
+                            now_level[i] = 0;
+                        }
+                    }
+                }
+                else {
+                    now_level[j - 1] ++;
+                }
+            }
+
+            if (now == tmp2) {
+                pre = tmp2;
+                now = tmp1;
+            }
+            else {
+                pre = tmp1;
+                now = tmp2;
+            }
+        }
+
+        // j == n
+        {
+            size_t j = n;
+            if (0 < j - 2) {
+                // clean array tmp
+                arry_size = pow(2, (j - 2) * bits[c]);
+                memset(now, 0, arry_size * sizeof(int));
+            }
+
+            for (size_t i = 0; i < j; i++) {
+                now_level[i] = 0;
+            }
+            for (size_t i = j; i < 14; i++) {
+                now_level[i] = -1;
+            }
+
+            while (true) {
+                for (size_t i = 0; i < j - 1; i++) {
+                    pre_level[i] = 0;
+                }
+                for (size_t i = j - 1; i < 14; i++) {
+                    pre_level[i] = -1;
+                }
+
+                while (true) {
+                    // proc
+                    int pre_index = getIndex(pre_level, c);
+
+                    if (pre[pre_index]) {
+                        long long multiplier = getMultiplier(pre_level, now_level, c);
+
+                        if (0 < multiplier) {
+                            long long product = pre[pre_index];
+                            product *= multiplier;
+                            product %= MOD;
+
+                            ret += product;
+                            ret %= MOD;
+                        }
+                    }
+
+                    // 遍历 pre_level
+                    if (c - 1 == pre_level[j - 2]) {
+                        int pos = j - 2;
+                        while (c - 1 == pre_level[pos] && pos >= 0) {
+                            pos --;
+                        }
+
+                        if (0 > pos) {
+                            break;
+                        }
+                        else {
+                            pre_level[pos] ++;
+                            for (size_t i = pos + 1; i < j - 1; i++) {
+                                pre_level[i] = 0;
+                            }
+                        }
+                    }
+                    else {
+                        pre_level[j - 2] ++;
+                    }
+                }
+
+                // 遍历 now_level
+                if (c - 1 == now_level[j - 1]) {
+                    int pos = j - 1;
+                    while (c - 1 == now_level[pos] && pos >= 0) {
+                        pos --;
+                    }
+
+                    if (0 < pos) {
+                        break;
+                    }
+                    else {
+                        now_level[pos] ++;
+                        for (size_t i = pos + 1; i < j; i++) {
+                            now_level[i] = 0;
+                        }
+                    }
+                }
+                else {
+                    now_level[j - 1] ++;
+                }
+            }
+        }
+    }
 
     printf("%lld\n", ret);
 
