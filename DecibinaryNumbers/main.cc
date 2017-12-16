e609eb7c9e7f4b8bc6c32a199392b8b7702d718b
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2017-12-16 20:06:11 +0800

    update

--- DecibinaryNumbers/main.cc
+++ DecibinaryNumbers/main.cc
@@ -1,9 +1,26 @@
+//
+//  main.cpp
+//  DecibinaryNumbers
+//
+//  Created by 吴星煜 on 2017/12/16.
+//  Copyright © 2017年 吴星煜. All rights reserved.
+//
+//
+//  main.cpp
+//  DecibinaryNumbers
+//
+//  Created by 吴星煜 on 2017/12/16.
+//  Copyright © 2017年 吴星煜. All rights reserved.
+//
+
 #include <stdio.h>
 #include <string>
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
 #include <math.h>
+
+#define DEBUG       0
 
 using namespace std;
 
@@ -12,6 +29,8 @@ public:
     unsigned int            value;
     unsigned long long      count;
 };
+
+vector< vector<class Cnt> >     vec(54);
 
 unsigned long long mins[] = {0,9,27,63,135,279,567,1143,2295,4599,9207,18423,36855,73719,147447,294903,589815,1179639,2359287,4718583,9437175,18874359,37748727,75497463,150994935,301989879,603979767,1207959543,2415919095,4831838199,9663676407,19327352823,38654705655,77309411319,154618822647,309237645303,618475290615,1236950581239,2473901162487,4947802324983,9895604649975,19791209299959,39582418599927,79164837199863,158329674399735,316659348799479,633318697598967,1266637395197943,2533274790395895,5066549580791799,10133099161583607};
 vector<unsigned long long>  min_vec(mins, mins + 51);
@@ -54,6 +73,11 @@ unsigned long long getCount(unsigned long long number, int digit) {
             int vec_size = vec[j].size();
             if (0 < vec_size) {
                 if (remain >= vec[j][0].value && remain <= vec[j][vec_size - 1].value) {
+/*
+                    if (remain != vec[j][remain - vec[j][0].value].value) {
+                        ret += vec[j][remain - vec[j][0].value].count;
+                    }
+*/
                     ret += vec[j][remain - vec[j][0].value].count;
                 }
             }
@@ -63,12 +87,22 @@ unsigned long long getCount(unsigned long long number, int digit) {
     return ret;
 }
 
-vector< vector<class Cnt> >     vec(54);
-
 int main() {
-    int q;
+#if DEBUG
+    FILE * fp;
+    fp = fopen("input.txt", "r");
 
+    FILE * fp_out;
+    fp_out = fopen("output.txt", "w");
+#endif
+
+    int q;
+#if DEBUG
+    fscanf(fp, "%d", &q);
+#else
     scanf("%d", &q);
+#endif
+
 
     vector<long long> input(q);
     vector<long long> sorted_input(q);
@@ -76,7 +110,12 @@ int main() {
 
     for (size_t i = 0; i < q; i++) {
         long long x;
+
+#if DEBUG
+        fscanf(fp, "%lld", &x);
+#else
         scanf("%lld", &x);
+#endif
 
         input[i] = x;
         sorted_input[i] = x;
@@ -86,18 +125,15 @@ int main() {
     sort(sorted_input.begin(), sorted_input.end());
 
     int iIn = 0;
-    unsigned long long xth = 1;
-    unsigned long long number = 1;
-
-    class Cnt zero;
-    zero.value = 0;
-    zero.count = 1;
-
-    vec[1].push_back(zero);
+    unsigned long long xth = 0;
+    unsigned long long number = 0;
 
     while (iIn < q) {
         int min_digit = getMinDigit(number);
         int max_digit = getMaxDigit(number);
+
+        min_digit = min_digit > 0 ? min_digit : 1;
+        max_digit = max_digit > 0 ? max_digit : 1;
 
         int now_digit = min_digit;
         while (now_digit <= max_digit) {
@@ -110,31 +146,54 @@ int main() {
                 new_cnt.count = getCount(number, now_digit);
             }
 
-            xth += new_cnt.count;
-
-            while (xth >= sorted_input[iIn]) {
+            while (xth + new_cnt.count >= sorted_input[iIn]) {
                 // hit
-                
+                /*
+                 if (1 == now_digit) {
+                 sorted_output[iIn] += '0' + number;
+                 }
+                 else {
+                 unsigned long long offset = sorted_input[iIn] - xth;
 
+                 }
+                 */
+                sorted_output[iIn] = to_string(number);
                 iIn ++;
+                if (iIn >= q) {
+                    break;
+                }
             }
+
+            if (iIn >= q) {
+                break;
+            }
+
+            xth += new_cnt.count;
 
             vec[now_digit].push_back(new_cnt);
 
             now_digit ++;
-
         }
+        number ++;
     }
 
     // output
     for (size_t i = 0; i < q; i++) {
         long long x = input[i];
 
-        vector<long long>::iterator loc = upper_bound(sorted_input.begin(), sorted_input.end(), x);
+        vector<long long>::iterator loc = lower_bound(sorted_input.begin(), sorted_input.end(), x);
         int pos = loc - sorted_input.begin();
-
+#if DEBUG
+        fprintf(fp_out, "%s\n", sorted_output[pos].c_str());
+#else
         printf("%s\n", sorted_output[pos].c_str());
+#endif
     }
+
+#if DEBUG
+    fclose(fp);
+    fclose(fp_out);
+#endif
 
     return 0;
 }
