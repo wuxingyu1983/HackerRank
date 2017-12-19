63a63146def08883433e255f10a5dcfedc2e97f1
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2017-12-19 11:35:46 +0800

    70.00 points

--- HexagonalGrid/main.c
+++ HexagonalGrid/main.c
@@ -3,22 +3,46 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define DEBUG       1
+
 int main() {
+#if DEBUG
+    FILE * fp;
+    fp = fopen("input.txt", "r");
+
+    FILE * fp_out;
+    fp_out = fopen("output.txt", "w");
+#endif
+
     int t;
 
+#if DEBUG
+    fscanf(fp, "%d", &t);
+#else
     scanf("%d", &t);
+#endif
 
     char * * output = (char * *)malloc(t * sizeof(char *));
 
     for (size_t i = 0; i < t; i++) {
         int n;
+
+#if DEBUG
+        fscanf(fp, "%d", &n);
+#else
         scanf("%d", &n);
+#endif
 
         char * grid1 = malloc(n + 2);
         char * grid2 = malloc(n + 2);
 
+#if DEBUG
+        fscanf(fp, "%s", grid1 + 1);
+        fscanf(fp, "%s", grid2 + 1);
+#else
         scanf("%s", grid1 + 1);
         scanf("%s", grid2 + 1);
+#endif
 
         unsigned char * * cnt = (unsigned char * *)malloc((n + 1) * sizeof(unsigned char *));
         for (size_t j = 0; j < n + 1; j++) {
@@ -110,6 +134,14 @@ int main() {
                     if ('0' == grid1[j - 1]) {
                         cnt[j][j] = cnt[j - 2][j - 1];
                     }
+
+                    if ('0' == grid2[j - 1]) {
+                        cnt[j][j] += cnt[j - 1][j - 2];
+                    }
+
+                    if (0 < cnt[j][j]) {
+                        cnt[j][j] = 1;
+                    }
                 }
             }
         }
@@ -133,8 +165,17 @@ int main() {
     }
 
     for (size_t i = 0; i < t; i++) {
+#if DEBUG
+        fprintf(fp_out, "%s\n", output[i]);
+#else
         printf("%s\n", output[i]);
+#endif
     }
+
+#if DEBUG
+    fclose(fp);
+    fclose(fp_out);
+#endif
 
     return 0;
 }
