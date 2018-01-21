1019ef3821d4e07e0fc706a05b50d8df8694bbf1
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2018-01-21 21:33:54 +0800

    76.36 points

--- TravelAroundTheWorld/main.cc
+++ TravelAroundTheWorld/main.cc
@@ -1,3 +1,11 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 吴星煜 on 2018/1/21.
+//  Copyright © 2018年 吴星煜. All rights reserved.
+//
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -33,6 +41,9 @@ int main() {
 
     for (size_t i = 0; i < n; i++) {
         scanf("%lld", &a[i]);
+        if (a[i] > c) {
+            a[i] = c;
+        }
     }
 
     for (size_t i = 0; i < n; i++) {
@@ -43,7 +54,7 @@ int main() {
 
     // find first loop
     int tmp = 0;
-    while (tmp < n) {
+    while (tmp <= n) {
         if (0 == cont_seq.size()) {
             Seq s(0, 0);
             cont_seq.push(s);
@@ -58,14 +69,21 @@ int main() {
             }
             else {
                 last.start = index;
+
+                if (0 <= last.bonus && a[index] - b[index] > last.bonus) {
+                    last.remain += last.bonus;
+                }
+                else {
+                    last.remain += a[index] - b[index];
+                }
+                last.remain = last.remain > c ? c : last.remain;
+
                 if (last.bonus < 0) {
                     last.bonus = c - a[index];
                 }
                 else {
                     last.bonus = (last.bonus > c - a[index]) ? (c - a[index]) : last.bonus;
                 }
-                last.remain += last.bonus;
-                last.remain = last.remain > c ? c : last.remain;
 
                 cont_seq.pop();
 
@@ -80,30 +98,30 @@ int main() {
                         }
                         else {
                             Seq next = cont_seq.top();
+                            if (0 <= next.bonus && next.bonus < (last.remain + a[last.end] - b[last.end])) {
+                                last.remain = next.remain + next.bonus;
+                            }
+                            else {
+                                last.remain = next.remain + last.remain + a[last.end] - b[last.end];
+                            }
+                            last.remain = last.remain > c ? c : last.remain;
+
                             long long tmp_bonus = c - last.remain - a[last.end];
                             if (0 > tmp_bonus) {
                                 tmp_bonus = 0;
                             }
 
-                            if (last.remain > tmp_bonus) {
-                                last.remain = next.remain + tmp_bonus;
-                            }
-                            else {
-                                last.remain = next.remain + last.remain;
-                            }
-                            last.remain = last.remain > c ? c : last.remain;
-
-                            last.end = next.end;
-
-                            if (0 >= last.bonus && last.bonus < tmp_bonus) {
-                                tmp_bonus = last.bonus;
-                            }
-
-                            if (0 >= next.bonus && next.bonus < tmp_bonus) {
+                            if (0 <= next.bonus && next.bonus < tmp_bonus) {
                                 tmp_bonus = next.bonus;
                             }
 
+                            if (0 <= last.bonus && last.bonus < tmp_bonus) {
+                                tmp_bonus = last.bonus;
+                            }
+
                             last.bonus = tmp_bonus;
+
+                            last.end = next.end;
 
                             cont_seq.pop();
                         }
@@ -113,13 +131,105 @@ int main() {
                         cont_seq.push(last);
                     }
                 }
-             }
+            }
         }
 
         tmp ++;
     }
 
+    if (1 == cont_seq.size()) {
+        Seq last = cont_seq.top();
+        cont_seq.pop();
 
+        tmp = 0;
+        while (tmp < n) {
+            if (0 == cont_seq.size()) {
+                Seq s(last.start, last.start);
+                cont_seq.push(s);
+            }
+            else {
+                Seq last = cont_seq.top();
+
+                int index = (last.start - 1 + n) % n;
+                if (a[index] < b[index]) {
+                    Seq s(index, index);
+                    cont_seq.push(s);
+                }
+                else {
+                    last.start = index;
+
+                    if (0 <= last.bonus && a[index] - b[index] > last.bonus) {
+                        last.remain += last.bonus;
+                    }
+                    else {
+                        last.remain += a[index] - b[index];
+                    }
+                    last.remain = last.remain > c ? c : last.remain;
+
+                    if (last.bonus < 0) {
+                        last.bonus = c - a[index];
+                    }
+                    else {
+                        last.bonus = (last.bonus > c - a[index]) ? (c - a[index]) : last.bonus;
+                    }
+
+                    cont_seq.pop();
+
+                    if (0 == cont_seq.size()) {
+                        cont_seq.push(last);
+                    }
+                    else {
+                        while (0 < cont_seq.size()) {
+                            if (last.remain + a[last.end] < b[last.end]) {
+                                cont_seq.push(last);
+                                break;
+                            }
+                            else {
+                                Seq next = cont_seq.top();
+
+                                if (0 <= next.bonus && next.bonus < (last.remain + a[last.end] - b[last.end])) {
+                                    last.remain = next.remain + next.bonus;
+                                }
+                                else {
+                                    last.remain = next.remain + last.remain + a[last.end] - b[last.end];
+                                }
+                                last.remain = last.remain > c ? c : last.remain;
+
+                                long long tmp_bonus = c - last.remain - a[last.end];
+                                if (0 > tmp_bonus) {
+                                    tmp_bonus = 0;
+                                }
+
+                                if (0 <= next.bonus && next.bonus < tmp_bonus) {
+                                    tmp_bonus = next.bonus;
+                                }
+
+                                if (0 <= last.bonus && last.bonus < tmp_bonus) {
+                                    tmp_bonus = last.bonus;
+                                }
+
+                                last.bonus = tmp_bonus;
+
+                                last.end = next.end;
+
+                                cont_seq.pop();
+                            }
+                        }
+
+                        if (0 == cont_seq.size()) {
+                            cont_seq.push(last);
+                        }
+                    }
+                }
+            }
+
+            if (1 == cont_seq.size()) {
+                ret ++;
+            }
+
+            tmp ++;
+        }
+    }
 
     printf("%d\n", ret);
 
