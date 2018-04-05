fe4a789c55615a8d7525dfd364334b3a72420059
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2018-04-05 11:11:36 +0800

    modify

--- BeautifulQuadruples/main.cc
+++ BeautifulQuadruples/main.cc
@@ -8,10 +8,13 @@
 
 using namespace std;
 
-#define MAX      4096       // 0xFFF
+#define MAX      4096       // 0 - 0xFFF
 
 int lcnt[MAX][MAX];
 int rcnt[MAX][MAX];
+
+int lsum[MAX];
+int rsum[MAX];
 
 int main() {
     vector<int> a;
@@ -23,47 +26,61 @@ int main() {
 
     for (int s = 0; s < MAX; s++) {
         int z;
-        for (int y = 1; y <= a[3]; y++) {
+        for (int y = 1; y <= a[2]; y++) {
             z = s ^ y;
-            if (y <= z) {
+            if (y <= z && z <= a[3]) {
                 lcnt[1][s] ++;
             }
         }
 
-        for (int y = 2; y <= a[3]; y++) {
+        lsum[1] += lcnt[1][s];
+
+        for (int y = 2; y <= a[2]; y++) {
             z = s ^ y;
-            if (y <= z) {
+            if (y <= z && z <= a[3]) {
                 lcnt[y][s] = lcnt[y - 1][s] - 1;
             }
             else {
                 lcnt[y][s] = lcnt[y - 1][s];
             }
+
+            lsum[y] += lcnt[y][s];
         }
     }
 
     for (int s = 0; s < MAX; s++) {
         int w;
-        for (int x = 1; x <= a[2]; x++) {
+        for (int x = 1; x <= a[1]; x++) {
             w = s ^ x;
-            if (w <= x) {
-                rcnt[a[2]][s] ++;
+            if (w <= x && w <= a[0]) {
+                rcnt[a[1]][s] ++;
             }
         }
 
-        for (int x = a[2] - 1; x > 0; x--) {
+        rsum[a[1]] += rcnt[a[1]][s];
+
+        for (int x = a[1] - 1; x > 0; x--) {
             w = s ^ x;
-            if (w <= x) {
+            if (w <= x && w <= a[0]) {
                 rcnt[x][s] = rcnt[x + 1][s] - 1;
             }
             else {
                 rcnt[x][s] = rcnt[x + 1][s];
             }
+
+            rsum[x] += rcnt[x][s];
         }
     }
 
     unsigned long long ret = 0;
 
-    
+    for (size_t i = 1; i <= a[1]; i++) {
+        for (size_t j = 0; j < MAX; j++) {
+            if (rcnt[i][j]) {
+                ret += (unsigned long long)rcnt[i][j] * (unsigned long long)(lsum[j] - lcnt[i][j]);
+            }
+        }
+    }
 
     printf("%llu\n", ret);
 
