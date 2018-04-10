7cde6f0818630eb80c653b6a0d5404e8147f3a07
吴星煜 <wuxingyu@wuxingyudeMac-mini.local>
2018-04-10 19:36:46 +0800

    init

---
+++ PrimeDigitSums/main.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+#define MOD     1000000007
+
+int main() {
+    int q;
+    scanf("%d", &q);
+
+    
+
+    return 0;
+}
