88e407d48a8157a7080e71f196cb4d53f5d3362d
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2018-08-23 01:14:55 +0800

    69.19 points

--- PointsInAPlane/main.cc
+++ PointsInAPlane/main.cc
@@ -2,6 +2,14 @@
 //  main.cpp
 //  test
 //
+//  Created by 吴星煜 on 2018/8/22.
+//  Copyright © 2018年 吴星煜. All rights reserved.
+//
+
+//
+//  main.cpp
+//  test
+//
 //  Created by wuxingyu on 2018/8/22.
 //  Copyright © 2018年 wuxingyu. All rights reserved.
 //
@@ -13,50 +21,72 @@
 #include <algorithm>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+#include <fstream>
 
-#define MOD 1000000007
-#define MAX (1 << 16)
+#define MOD         1000000007
+#define MAX         (1 << 16)
+#define DEBUG       0
 
 using namespace std;
 
-int cnt[9][MAX]; // 8 - max turns
+int cnt[50][9][MAX]; // 8 - max turns
 
 class Point
 {
-  public:
+public:
     int x, y;
 };
 
-void getMinMoves(vector<Point> &p)
+void recu_func(int index, vector<Point> &p, int p1, int p2, int origin, int mask, int m, int count)
 {
-    long long ret = 0;
+    if (0 > p2) {
+        // new line
+        for (p2 = p1; p2 < p.size(); p2++) {
+            if (p2 != p1) {
+                if (origin & (1 << p2)) {
+                    int new_mask = mask | (1 << p2);
+                    int dst = origin ^ new_mask;
+                    cnt[index][m + 1][dst] += count;
+                    cnt[index][m + 1][dst] %= MOD;
 
-    int max = (1 << p.size()) - 1;
-    for (int i = 0; i <= 8; i++)
-    {
-        for (int j = 0; j <= max; j++)
-        {
-            cnt[i][j] = 0;
+                    recu_func(index, p, p1, p2, origin, new_mask, m, count);
+                }
+            }
         }
     }
+    else {
+        // find point on p1 => p2
+        for (int p3 = p2; p3 < p.size(); p3++) {
+            if (p3 != p2) {
+                if (origin & (1 << p3)) {
+                    if ((p[p2].y - p[p1].y) * (p[p3].x - p[p1].x) == (p[p3].y - p[p1].y) * (p[p2].x - p[p1].x)) {
+                        // p1,p2,p3 on the same line
+                        int new_mask = mask | (1 << p3);
+                        int dst = origin ^ new_mask;
+                        cnt[index][m + 1][dst] += count;
+                        cnt[index][m + 1][dst] %= MOD;
 
-    cnt[0][max] = 1;
+                        recu_func(index, p, p1, p3, origin, new_mask, m, count);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void getMinMoves(int index, vector<Point> &p)
+{
+    int max = (1 << p.size()) - 1;
+
+    cnt[index][0][max] = 1;
 
     for (int m = 0; m <= 8; m++)
     {
-        if (cnt[m][0])
+        if (cnt[index][m][0])
         {
             // end, process ret
-            ret = 1;
-            for (int i_m = m; i_m >= 1; i_m--)
-            {
-                ret *= i_m;
-            }
-
-            ret *= cnt[m][0];
-            ret %= MOD;
-
-            cout << m << " " << ret << "\n";
+            cout << m << " " << cnt[index][m][0] << "\n";
 
             break;
         }
@@ -64,76 +94,19 @@ void getMinMoves(vector<Point> &p)
         {
             for (int i = 1; i <= max; i++)
             {
-                if (cnt[m][i])
+                if (cnt[index][m][i])
                 {
                     int origin = i;
-                    int j = 0;
-                    for (j = 0; j < p.size(); j++)
+
+                    for (int p1 = 0; p1 < p.size(); p1++)
                     {
-                        if (origin == (1 << j))
-                        {
-                            // only 1 point
-                            cnt[m + 1][0] += cnt[m][i];
-                            cnt[m + 1][0] %= MOD;
-                            break;
-                        }
-                    }
+                        if (origin & (1 << p1)) {
+                            int mask = 1 << p1;
+                            int dst = origin ^ mask;
+                            cnt[index][m + 1][dst] += cnt[index][m][i];
+                            cnt[index][m + 1][dst] %= MOD;
 
-                    if (j == p.size())
-                    {
-                        vector<int> masks;
-
-                        for (int p1 = 0; p1 < p.size(); p1++)
-                        {
-                            for (int p2 = 0; p2 < p.size(); p2++)
-                            {
-                                if (p1 != p2)
-                                {
-                                    if (origin & (1 << p1) && origin & (1 << p2))
-                                    {
-                                        int mask = (1 << p1) | (1 << p2);
-
-                                        for (int p3 = 0; p3 < p.size(); p3++)
-                                        {
-                                            if (p3 != p1 && p3 != p2 && (origin & (1 << p3)))
-                                            {
-                                                if ((p[p2].x - p[p1].x) * (p[p3].y - p[p1].y) == (p[p3].x - p[p1].x) * (p[p2].y - p[p1].y))
-                                                {
-                                                    // on one line
-                                                    mask |= (1 << p3);
-                                                }
-                                            }
-                                        }
-
-                                        if (0 == masks.size())
-                                        {
-                                            masks.push_back(mask);
-                                            int dst = origin ^ mask;
-                                            cnt[m + 1][dst] += cnt[m][origin];
-                                            cnt[m + 1][dst] %= MOD;
-                                        }
-                                        else
-                                        {
-                                            int i_m = 0;
-                                            for (i_m = 0; i_m < masks.size(); i_m++)
-                                            {
-                                                if (masks[i_m] == mask)
-                                                {
-                                                    break;
-                                                }
-                                            }
-
-                                            if (i_m == masks.size())
-                                            {
-                                                masks.push_back(mask);
-                                                int dst = origin ^ mask;
-                                                cnt[m + 1][dst] += cnt[m][origin];
-                                                cnt[m + 1][dst] %= MOD;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            recu_func(index, p, p1, -1, origin, mask, m, cnt[index][m][i]);
                         }
                     }
                 }
@@ -144,29 +117,45 @@ void getMinMoves(vector<Point> &p)
 
 int main()
 {
+#if DEBUG
+    ifstream inFile;
+    inFile.open("input.txt");
+#endif
+
     int t;
+#if DEBUG
+    inFile >> t;
+#else
     cin >> t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#endif
 
     for (int i_t = 0; i_t < t; i_t++)
     {
         int n;
+#if DEBUG
+        inFile >> n;
+#else
         cin >> n;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
+#endif
         vector<Point> p(n);
 
         for (int i_n = 0; i_n < n; i_n++)
         {
             int x, y;
+#if DEBUG
+            inFile >> x >> y;
+#else
             cin >> x >> y;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#endif
 
             p[i_n].x = x;
             p[i_n].y = y;
         }
 
-        getMinMoves(p);
+        getMinMoves(i_t, p);
     }
 
     return 0;
