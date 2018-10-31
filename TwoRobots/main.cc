da02026024082a0667c48993243ec03c1857f3b0
wuxingyu <wuxingyu@wuxingyudeMacBook-Pro-2.local>
2018-10-31 18:22:52 +0800

    50.00 points

--- TwoRobots/main.cc
+++ TwoRobots/main.cc
@@ -36,7 +36,7 @@ int main()
     cin >> t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 #endif
-    
+
     for (int i_t = 0; i_t < t; i_t++)
     {
         int m, n;
@@ -49,8 +49,9 @@ int main()
         memset(d, 0, sizeof(int) * 2 * (m + 1) * (m + 1));
 
         int index = 0;
+        int last_p = 0;
 
-        for (int i_n = 0; i_n < n; i_n ++)
+        for (int i_n = 0; i_n < n; i_n++)
         {
             int ma, mb;
 #if DEBUG
@@ -62,20 +63,23 @@ int main()
             if (0 == i_n)
             {
                 d[index][0][mb] = abs(ma - mb);
+                last_p = mb;
             }
             else
             {
-                for (int b = 0; b <= m; b ++)
+                for (int a = 0; a < mb; a++)
                 {
-                    for (int a = 0; a <= b; a ++)
-                    {
-                        d[index][a][b] = 0;
-                    }
+                    d[index][a][mb] = 0;
                 }
 
-                for (int b = 0; b <= m; b++)
+                for (int b = mb; b <= m; b++)
                 {
-                    for (int a = 0; a <= b; a++)
+                    d[index][mb][b] = 0;
+                }
+
+                int b = last_p;
+                {
+                    for (int a = 0; a < b; a++)
                     {
                         if (d[1 - index][a][b])
                         {
@@ -124,23 +128,79 @@ int main()
                         }
                     }
                 }
+
+                int a = last_p;
+                {
+                    for (int b = last_p; b <= m; b++)
+                    {
+                        if (d[1 - index][a][b])
+                        {
+                            // b => ma => mb
+                            int tmp = d[1 - index][a][b] + abs(ma - mb) + abs(b - ma);
+
+                            if (a <= mb)
+                            {
+                                if (0 == d[index][a][mb] || d[index][a][mb] > tmp)
+                                {
+                                    d[index][a][mb] = tmp;
+                                }
+                            }
+                            else
+                            {
+                                if (0 == d[index][mb][a] || d[index][mb][a] > tmp)
+                                {
+                                    d[index][mb][a] = tmp;
+                                }
+                            }
+
+                            // a => ma => mb
+                            if (0 == a)
+                            {
+                                tmp = d[1 - index][a][b] + abs(ma - mb);
+                            }
+                            else
+                            {
+                                tmp = d[1 - index][a][b] + abs(ma - mb) + abs(a - ma);
+                            }
+
+                            if (b <= mb)
+                            {
+                                if (0 == d[index][b][mb] || d[index][b][mb] > tmp)
+                                {
+                                    d[index][b][mb] = tmp;
+                                }
+                            }
+                            else
+                            {
+                                if (0 == d[index][mb][b] || d[index][mb][b] > tmp)
+                                {
+                                    d[index][mb][b] = tmp;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                last_p = mb;
             }
 
             if (i_n == n - 1)
             {
                 int ret = 0;
 
-                for (int b = 0; b <= m; b++)
+                for (int a = 0; a < mb; a++)
                 {
-                    for (int a = 0; a <= b; a++)
+                    if (d[index][a][mb] && (0 == ret || ret > d[index][a][mb]))
                     {
-                        if (d[index][a][b])
-                        {
-                            if (0 == ret || ret > d[index][a][b])
-                            {
-                                ret = d[index][a][b];
-                            }
-                        }
+                        ret = d[index][a][mb];
+                    }
+                }
+
+                for (int b = mb; b <= m; b++)
+                {
+                    if (d[index][mb][b] && (0 == ret || ret > d[index][mb][b]))
+                    {
+                        ret = d[index][mb][b];
                     }
                 }
 
