76ff9f441243b8fb61d24106a3e2303560b449b6
wuxingyu <wuxingyu1983@gmail.com>
2019-03-21 20:42:55 +0800

    init

---
+++ ChoosingWhiteBalls/main.cc
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    return 0;
+}
