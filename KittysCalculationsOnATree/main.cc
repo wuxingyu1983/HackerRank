bf69989455926156f07fab53c1907d47d4367e99
wuxingyu1983@gmail.com <wuxingyu1983@gmail.com>
2020-02-20 22:17:19 +0800

    52.00 points

---
+++ KittysCalculationsOnATree/main.cc
@@ -0,0 +1,155 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define DEBUG   0
+#define MAX_N   200002
+#define MOD     1000000007
+
+class node
+{
+public:
+    int parent;
+    vector<int> children;
+    long long sum1;     // ∑ua
+    long long sum2;     // ∑u
+
+    node()
+    {
+        parent = -1;
+        sum1 = 0;
+        sum2 = 0;
+    }
+} tree[MAX_N];
+
+void build_tree(multimap<int, int> &edges, int curr)
+{
+    for (multimap<int, int>::iterator it = edges.equal_range(curr).first; it != edges.equal_range(curr).second; ++it)
+    {
+        if (0 <= tree[(*it).second].parent)
+        {
+            // in the tree already
+        }
+        else
+        {
+            tree[(*it).second].parent = curr;
+            tree[curr].children.push_back((*it).second);
+
+            build_tree(edges, (*it).second);
+        }
+    }
+}
+
+void getSubSum(set<int>& sset, int curr, long long & sum)
+{
+    tree[curr].sum1 = 0;
+    tree[curr].sum2 = 0;
+
+    if (sset.end() != sset.find(curr))
+    {
+        // find
+        tree[curr].sum2 = curr;
+    }
+
+    if (0 < tree[curr].children.size())
+    {
+        for (vector<int>::iterator it = tree[curr].children.begin(); it != tree[curr].children.end(); it++)
+        {
+            getSubSum(sset, *it, sum);
+
+            sum += (tree[*it].sum1 + tree[*it].sum2) * tree[curr].sum2 + tree[curr].sum1 * tree[*it].sum2;
+
+            sum %= MOD;
+
+            tree[curr].sum1 += tree[*it].sum1 + tree[*it].sum2;
+            tree[curr].sum1 %= MOD;
+            tree[curr].sum2 += tree[*it].sum2;
+            tree[curr].sum2 %= MOD;
+        }
+    }
+}
+
+int main()
+{
+#if DEBUG
+    ifstream inFile;
+    inFile.open("input.txt");
+#endif
+
+    int n, q;
+ #if DEBUG
+    inFile >> n >> q;
+#else
+    cin >> n >> q;
+#endif
+
+    multimap<int, int> edges;
+    for (size_t i = 0; i < n - 1; i++)
+    {
+        int a, b;
+#if DEBUG
+        inFile >> a >> b;
+#else
+        cin >> a >> b;
+#endif
+
+        edges.insert(pair<int, int>(a, b));
+        edges.insert(pair<int, int>(b, a));
+    }
+
+    // build tree
+    int root = 1;
+    tree[root].parent = 0;
+    build_tree(edges, root);
+
+    for (size_t i = 0; i < q; i++)
+    {
+        int k;
+#if DEBUG
+        inFile >> k;
+#else
+        cin >> k;
+#endif       
+
+        set<int> sset;
+        for (size_t j = 0; j < k; j++)
+        {
+            int elem;
+#if DEBUG
+            inFile >> elem;
+#else
+            cin >> elem;
+#endif
+            sset.insert(elem);
+        }
+
+        if (0 == k)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            long long sum = 0;
+
+            getSubSum(sset, root, sum);
+
+            cout << sum << endl;
+        }
+    }
+
+#if DEBUG
+    inFile.close();
+#endif
+
+    return 0;
+}
