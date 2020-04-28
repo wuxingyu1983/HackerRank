b82ff79f64ef0f5fef64fc59cda80cdeba6a88a4
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro.local>
2020-04-29 02:14:19 +0800

    4.28

--- LongestModPath/main.cc
+++ LongestModPath/main.cc
@@ -12,21 +12,32 @@
 
 using namespace std;
 
-#define DEBUG   0
-#define MAX_N   100000
+#define DEBUG           0
+#define MAX_N           100000
 
-bool findCircle(vector< vector<int> > &rooms, int curr, int d, stack<int> &dfs, vector<int> &depth, vector<int> &circle)
+vector< vector<int> > rooms;        // 记录与房间 i 联通的房间数组
+multimap<long long, int> corridors; // key : a * 100000 + b , value : x
+stack<int> dfs_tree;                // 深度搜索树
+vector<int> depth;           // 该 room 在搜索树中的 depth, 0 - 不在树中
+vector<int> circle;                 // 记录回路
+vector<long long> circle_sum;
+vector<int> circle_pos;
+vector<long long> sumFromCircle;
+vector<int> ancestorInCircle;
+
+bool findCircle(int curr, int d)
 {
+    // curr 已经在 stack dfs 中了 
     bool ret = false;
 
     for (vector<int>::iterator it = rooms[curr].begin(); it != rooms[curr].end(); it++)
     {
         if (0 == depth[*it])
         {
-            dfs.push(*it);
+            dfs_tree.push(*it);
             depth[*it] = d + 1;
 
-            ret = findCircle(rooms, *it, d + 1, dfs, depth, circle);
+            ret = findCircle(*it, d + 1);
             if (ret)
             {
                 break;
@@ -45,8 +56,8 @@ bool findCircle(vector< vector<int> > &rooms, int curr, int d, stack<int> &dfs, 
 
                 for (size_t i = 0; i < circle.size(); i++)
                 {
-                    circle[i] = dfs.top();
-                    dfs.pop();
+                    circle[i] = dfs_tree.top();
+                    dfs_tree.pop();
                 }
 
                 ret = true;
@@ -54,10 +65,52 @@ bool findCircle(vector< vector<int> > &rooms, int curr, int d, stack<int> &dfs, 
         }
     }
 
-    dfs.pop();
+    dfs_tree.pop();
     depth[curr] = 0;
 
     return ret;
+}
+
+void procOtherRooms(int curr, int parent, int ancestor, long long sum)
+{
+    for (vector<int>::iterator it = rooms[curr].begin(); it != rooms[curr].end(); it++)
+    {
+        if (0 <= circle_pos[*it])
+        {
+            // it 在  circle 上
+        }
+        else if (parent == *it)
+        {
+            // 已经处理过了
+        }
+        else
+        {
+            if (0 > ancestor)
+            {
+                ancestor = curr;
+            }
+
+            ancestorInCircle[*it] = ancestor;
+
+            long long key = (long long)curr * (long long)MAX_N + (long long)(*it);
+            multimap<long long, int>::iterator it_map = corridors.find(key);
+            if (it_map != corridors.end())
+            {
+                sumFromCircle[*it] = sum + it_map->second;
+            }
+            else
+            {
+                key = (long long)(*it) * (long long)MAX_N + (long long)curr;
+                it_map = corridors.find(key);
+                if (it_map != corridors.end())
+                {
+                    sumFromCircle[*it] = sum - it_map->second;
+                }
+            }
+
+            procOtherRooms(*it, curr, ancestor, sumFromCircle[*it]);
+        }
+    }
 }
 
 int main()
@@ -74,8 +127,7 @@ int main()
     cin >> n;
 #endif
 
-    vector< vector<int> > rooms(n + 1);       // 记录与房间 i 联通的房间数组
-    multimap<long long, int> corridors;     // key : a * 100000 + b , value : x
+    rooms.resize(n + 1);
 
     for (size_t i_n = 0; i_n < n; i_n++)
     {
@@ -94,15 +146,76 @@ int main()
     }
 
     // find 回路
-    stack<int> dfs_tree;        // 深度搜索树
-    vector<int> depth(n + 1);   // 该 room 在搜索树中的 depth, 0 - 不在树中
-    vector<int> circle;         // 记录回路
+    depth.resize(n + 1);
 
     int curr = 1;
     dfs_tree.push(curr);
     depth[curr] = 1;
 
-    findCircle(rooms, curr, 1, dfs_tree, depth, circle);
+    findCircle(curr, 1);
+
+    // circle 中已经是 回路 了
+    // 计算 circle 顺时针的区间 score 和 ，顺时针 index 从小到大
+    circle_sum.resize(circle.size());
+    circle_pos.resize(n + 1);
+    circle_pos.assign(n + 1, -1);
+    for (size_t i_c = 0; i_c < circle.size(); i_c++)
+    {
+        circle_pos[circle[i_c]] = i_c;
+
+        long long key;
+        long long a, b;
+        if (i_c + 1 < circle.size())
+        {
+            a = circle[i_c]; 
+            b = circle[i_c + 1];
+        }
+        else
+        {
+            a = circle[i_c];
+            b = circle[0];
+        }
+
+        key = a * (long long)MAX_N + b;
+
+        multimap<long long, int>::iterator it = corridors.find(key);
+        if (it != corridors.end())
+        {
+            if (i_c + 1 < circle.size())
+            {
+                circle_sum[i_c + 1] = circle_sum[i_c] + it->second;
+            }
+            else
+            {
+                circle_sum[0] = circle_sum[i_c] + it->second;
+            }
+        }
+        else
+        {
+            key = b * (long long)MAX_N + a;
+            it = corridors.find(key);
+            if (it != corridors.end())
+            {
+                if (i_c + 1 < circle.size())
+                {
+                    circle_sum[i_c + 1] = circle_sum[i_c] - it->second;
+                }
+                else
+                {
+                    circle_sum[0] = circle_sum[i_c] - it->second;
+                }
+            }
+        }
+    }
+
+    // 计算 circle 到其他点的 score 和
+    sumFromCircle.resize(n + 1);
+    ancestorInCircle.resize(n + 1);
+
+    for (size_t i_c = 0; i_c < circle.size(); i_c++)
+    {
+        procOtherRooms(circle[i_c], -1, -1, 0);
+    }
 
     int q;
 #if DEBUG
