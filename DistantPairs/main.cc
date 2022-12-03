259d0d4ac8f7bb3a2320a7207f30f08e519f7997
吴星煜 <wuxingyu@wuxingyudeMacBook-Pro-2.local>
2022-12-03 11:38:54 +0800

    init

---
+++ DistantPairs/main.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+
+    return 0;
+}
